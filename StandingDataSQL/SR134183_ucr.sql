849fc2ecff757cf1413336d884846fab923e7696
JOHNSONMATTHEY\FernaI01 <ivan.fernandes@matthey.com>
2018-09-05 13:35:42 +0100

    changed datatype for stringts

--- StandingDataSQL/SR134183_ucr.sql
+++ StandingDataSQL/SR134183_ucr.sql
@@ -6,15 +6,15 @@ go
 
 --insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[date_value]) values ('LBMA','Reporting','LastRunTime',11,'22-May-2018 00:00:00 AM')
 
-insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[date_value]) values ('Reports','LBMA','LastRunTime',2,'22-May-2018 00:00:00 AM')
+insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[date_value]) values ('Reports','LBMA','LastRunTime',11,'28-August-2018 00:00:00 AM')
 
 go
 
-insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[date_value]) values ('Reports','LBMA','logLevel',2,'info')
+insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','LBMA','logLevel',2,'info')
 
 go
 
-insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[date_value]) values ('Reports','LBMA','logFile',2,'LBMA_Report.log')
+insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','LBMA','logFile',2,'LBMA_Report.log')
 
 go
 
@@ -22,7 +22,7 @@ select count(*) from USER_const_repository
 
 go
 
-select * from USER_const_repository where context like '%LBMA%'
+select * from USER_const_repository where sub_context like '%LBMA%'
 
 go
 
