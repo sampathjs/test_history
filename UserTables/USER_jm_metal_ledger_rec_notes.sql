724d6bdefbc8868954f31429e71686b58f5d15d2
saurabh.jain <saurabh.jain@kwa-analytics.com>
2018-10-30 17:47:23 +0530

    Updated sql for USER_jm_metal_ledger_rec_notes
    1. Added sql drop before creating the table
    2. Added index

--- UserTables/USER_jm_metal_ledger_rec_notes.sql
+++ UserTables/USER_jm_metal_ledger_rec_notes.sql
@@ -1,13 +1,28 @@
-create table USER_jm_metal_ledger_rec_notes
+BEGIN TRANSACTION
+
+IF OBJECT_ID('dbo.USER_jm_metal_ledger_rec_notes' , 'U') IS NOT NULL
+         DROP TABLE [dbo].[USER_jm_metal_ledger_rec_notes] 
+
+GO
+
+CREATE TABLE [dbo].[USER_jm_metal_ledger_rec_notes] (
+       [deal_num] [int] NULL,
+       [break_description] [varchar](255) NOT NULL
+) ON [PRIMARY]
+
+
+grant select, insert, update, delete on [dbo].[USER_jm_metal_ledger_rec_notes] to olf_user
+
+grant select on [dbo].[USER_jm_metal_ledger_rec_notes] to olf_readonly
+
+grant select, insert, update, delete on [dbo].[USER_jm_metal_ledger_rec_notes] to olf_user_manual
+
+GO
+
+CREATE CLUSTERED INDEX [USER_jm_ml_rec_index] ON [dbo].[USER_jm_metal_ledger_rec_notes]
 (
-deal_num int,
-break_description varchar(255)
+       [deal_num] ASC
 )
+GO
 
-grant select, insert, update, delete on USER_jm_metal_ledger_rec_notes to olf_user
-
-grant select on USER_jm_metal_ledger_rec_notes to olf_readonly
-
-grant select, insert, update, delete on USER_jm_metal_ledger_rec_notes to olf_user_manual
-go  
-
+COMMIT; 
