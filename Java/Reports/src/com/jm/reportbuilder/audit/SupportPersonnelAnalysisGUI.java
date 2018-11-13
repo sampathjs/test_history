f75071a7e72a7b69aa28f77e6a1b0a2e27da7362
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2018-11-13 11:20:39 +0000

    Added the Security Audit Report

---
+++ Java/Reports/src/com/jm/reportbuilder/audit/SupportPersonnelAnalysisGUI.java
@@ -0,0 +1,18 @@
+package com.jm.reportbuilder.audit;
+
+import com.olf.openjvs.IContainerContext;
+import com.olf.openjvs.IScript;
+import com.olf.openjvs.OConsole;
+import com.olf.openjvs.OException;
+import com.openlink.framework.utm.Common;
+
+@com.olf.openjvs.PluginCategory(com.olf.openjvs.enums.SCRIPT_CATEGORY_ENUM.SCRIPT_CAT_TRADE_INPUT)
+public class SupportPersonnelAnalysisGUI extends Common implements IScript {
+	
+	
+	
+	public void execute(IContainerContext context) throws OException {
+		try { handleEvent(context, "support_personnel_analysis", "Support Personnel Analysis"); }
+		catch (Throwable t) { OConsole.oprint(t.toString()); }
+	}
+}
