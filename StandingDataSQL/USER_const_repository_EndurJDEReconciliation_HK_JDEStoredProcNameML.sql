77a60763e8b2992bc57ae0fcdc05851b1ac936e6
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2018-11-28 09:17:17 +0000

    HK ML Reconciliation - Report Builder Data Sources

--- StandingDataSQL/USER_const_repository_EndurJDEReconciliation_HK_JDEStoredProcNameML.sql
+++ StandingDataSQL/USER_const_repository_EndurJDEReconciliation_HK_JDEStoredProcNameML.sql
@@ -1,2 +1,2 @@
-INSERT INTO USER_const_repository (context,sub_context,name,type,string_value)
-VALUES ('Reconciliation','EndurJDEReconciliation_HK','JDEStoredProcNameML',2,'getJDEMetals')
+INSERT [dbo].[USER_const_repository] ([context], [sub_context], [name], [type], [string_value], [double_value], [int_value], [date_value]) 
+VALUES(N'Reconciliation', N'EndurJDEReconciliation_HK', N'JDEStoredProcNameML', 2, N'getJDEMetals', 0, 0, CAST(0x0000000000000000 AS DateTime))
