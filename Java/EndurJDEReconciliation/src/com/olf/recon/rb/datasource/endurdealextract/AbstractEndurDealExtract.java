5af3af74ff639e3a534feed79053061593182f8a
Swati Khanna <Swati.Khanna@matthey.com>
2018-12-04 11:54:26 +0000

    AbstractEndurDealExtract.java edited online with Bitbucket

--- Java/EndurJDEReconciliation/src/com/olf/recon/rb/datasource/endurdealextract/AbstractEndurDealExtract.java
+++ Java/EndurJDEReconciliation/src/com/olf/recon/rb/datasource/endurdealextract/AbstractEndurDealExtract.java
@@ -17,6 +17,13 @@ public abstract class AbstractEndurDealExtract
 	protected int windowEndDate;
 	protected int lastTradeDate;
 	protected String region;
+  
+  
+    public AbstractEndurDealExtract(int windowStartDate, int windowEndDate)throws OException{
+		this.windowStartDate = windowStartDate;
+		this.windowEndDate = windowEndDate;
+		
+	}
 	
 	public AbstractEndurDealExtract(int windowStartDate, int windowEndDate, String region,int lastTradeDate)throws OException{
 		this.windowStartDate = windowStartDate;
