f2be91d005cb8e3e7d268fbc4099d28a4d4cd48a
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2019-01-23 14:09:14 +0000

    Correction for when we have Cash deals need the deal comments esle use the ab.reference

--- ReportBuilder/DS_Custom_Vostro_TL_byDate.sql
+++ ReportBuilder/DS_Custom_Vostro_TL_byDate.sql
@@ -22,7 +22,7 @@ SELECT  a.account_number
    , ab.ins_type
    , ate.event_num
     , ab.trade_date
-    , tn.line_text reference
+    , (CASE WHEN ab.ins_type= 27001 THEN tn.line_text ELSE ab.reference END) reference
     , ate.ins_seq_num
     , ab.start_date
     , ab.maturity_date  end_date
@@ -44,19 +44,19 @@ SELECT  a.account_number
     , COALESCE ( ativL.value, aiL.info_value ) Loco
  
 --                                                         , '$$Reporting_Date$$' as report_date
-                    FROM ab_tran ab 
+   FROM ab_tran ab 
    INNER JOIN ab_tran_event ate on ate.tran_num=ab.tran_num AND  ate.event_type=14  $$eventRange$$ 
    INNER JOIN ab_tran_event_settle ates ON (ate.event_num=ates.event_num) 
    INNER JOIN account a ON a.account_id=ates.ext_account_id AND a.account_number = '$$accountNumber$$'
    INNER JOIN trans_status abs ON (abs.trans_status_id=ab.tran_status AND abs.name IN ('Validated', 'Matured', 'Closeout'))
- LEFT JOIN tran_notepad tn ON tn.tran_num = ab.tran_num and tn.note_type =(CASE  WHEN ab.buy_sell= 0 THEN 20001 ELSE 20002 END)
-      LEFT JOIN ab_tran_info_view ativ ON ab.tran_num=ativ.tran_num AND ativ.type_name='JM_Transaction_Id'
-     LEFT JOIN ab_tran_info_view ativL ON ab.tran_num=ativL.tran_num AND ativL.type_name='Loco'
-     LEFT JOIN account_info ai ON a.account_id=ai.account_id AND ai.info_type_id in (SELECT ait.type_id FROM account_info_type ait WHERE ait.type_name='Reporting Unit' )
-     LEFT JOIN account_info aiL ON a.account_id=aiL.account_id AND aiL.info_type_id in (SELECT ait.type_id FROM account_info_type ait WHERE ait.type_name='Loco' )
-     LEFT JOIN idx_unit unit ON unit.unit_label=ai.info_value
+   LEFT JOIN tran_notepad tn ON tn.tran_num = ab.tran_num and tn.note_type =(CASE  WHEN ab.buy_sell= 0 THEN 20001 ELSE 20002 END)
+   LEFT JOIN ab_tran_info_view ativ ON ab.tran_num=ativ.tran_num AND ativ.type_name='JM_Transaction_Id'
+   LEFT JOIN ab_tran_info_view ativL ON ab.tran_num=ativL.tran_num AND ativL.type_name='Loco'
+   LEFT JOIN account_info ai ON a.account_id=ai.account_id AND ai.info_type_id in (SELECT ait.type_id FROM account_info_type ait WHERE ait.type_name='Reporting Unit' )
+   LEFT JOIN account_info aiL ON a.account_id=aiL.account_id AND aiL.info_type_id in (SELECT ait.type_id FROM account_info_type ait WHERE ait.type_name='Loco' )
+   LEFT JOIN idx_unit unit ON unit.unit_label=ai.info_value
  
-                              WHERE  ab.tran_status IN (SELECT trans_status_id FROM trans_status WHERE name in ('Validated', 'Matured', 'Closeout'))
-                                      AND ab.current_flag=1
-                                      AND ab.offset_tran_num<1
-                                     $$tradeRange$$
+   WHERE  ab.tran_status IN (SELECT trans_status_id FROM trans_status WHERE name in ('Validated', 'Matured', 'Closeout'))
+      AND ab.current_flag=1
+      AND ab.offset_tran_num<1
+      $$tradeRange$$
