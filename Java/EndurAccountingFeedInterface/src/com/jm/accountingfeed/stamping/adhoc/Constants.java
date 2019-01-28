22cce1b8370946eed0179dd968ef000cdb4ecb61
Arjit.Agrawal <arjit.agrawal@matthey.com>
2019-01-28 13:13:24 +0530

    code changes for adding a new ad-hoc task to update tran info - GL, ML field & sl_status column

---
+++ Java/EndurAccountingFeedInterface/src/com/jm/accountingfeed/stamping/adhoc/Constants.java
@@ -0,0 +1,32 @@
+package com.jm.stamping.adhoc;
+
+public class Constants {
+
+	protected static final String CONST_REPO_CONTEXT = "Util";
+	protected static final String CONST_REPO_SUB_CONTEXT = "LedgerFieldStampingAdhoc";
+	
+	public static final String USER_JM_SL_DOC_TRACKING_TABLE = "USER_jm_sl_doc_tracking";
+	
+	public static final String SUCCESS = "SUCCESS";
+	public static final String FAIL = "FAIL";
+	public static final String NO_CHANGE = "NO CHANGE";
+	
+	public static final String LEDGER_FIELD_NAME_IDENTIFIER = "ledger_field";
+	public static final String DEAL_LEDGER_FIELD_NAME_IDENTIFIER = "deal_ledger_field";
+	public static final String DOC_LEDGER_FIELD_NAME_IDENTIFIER = "doc_ledger_field";
+	public static final String LEDGER_FIELD_VALUE_INDETIFIER = "stamping_value";
+	public static final String DEAL_NUMS_INDETIFIER = "deal_nums";
+	public static final String DOC_NUMS_INDETIFIER = "doc_nums";
+	
+	public static final String OUTPUT_TABLE_COL_DEAL_NUM = "deal_num";
+	public static final String OUTPUT_TABLE_COL_DOC_NUM = "doc_num";
+	public static final String OUTPUT_TABLE_COL_PREV_VALUE = "prev_value";
+	public static final String OUTPUT_TABLE_COL_NEW_VALUE = "new_value";
+	public static final String OUTPUT_TABLE_COL_STATUS = "status";
+	public static final String OUTPUT_TABLE_COL_COMMENT = "comment";
+	
+	public static final String USER_SL_DOC_TRACKING_TABLE_COL_DOC_NUM = "document_num";
+	public static final String USER_SL_DOC_TRACKING_TABLE_COL_SL_STATUS = "sl_status";
+	public static final String USER_SL_DOC_TRACKING_TABLE_COL_LAST_UPDATE = "last_update";
+	public static final String USER_SL_DOC_TRACKING_TABLE_COL_PERSONNEL_ID = "personnel_id";
+}
