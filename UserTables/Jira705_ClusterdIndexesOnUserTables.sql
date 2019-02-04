321356d655aacf562f454f62580d1cce3c6888ab
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2019-02-04 13:25:56 +0000

    I’ve applied the indexes to UAT  - took 1 min 13 seconds to apply
    Remember these will be lost next time UAT is refreshed – unless these are added into emdash as post refresh steps for UAT…

---
+++ UserTables/Jira705_ClusterdIndexesOnUserTables.sql
@@ -0,0 +1,7 @@
+// Supplied by S Hindmarsh to improve efficiency for some of our user tables
+
+CREATE CLUSTERED INDEX CL_user_jm_jde_extract_data ON user_jm_jde_extract_data(deal_num) 
+
+CREATE CLUSTERED INDEX CL_User_jm_jde_extract_data_hist ON user_jm_jde_extract_data_hist(deal_num, entry_date, entry_time) WITH (DATA_COMPRESSION=PAGE)
+
+CREATE CLUSTERED INDEX CL_User_alert_log on user_alert_log(log_id)
