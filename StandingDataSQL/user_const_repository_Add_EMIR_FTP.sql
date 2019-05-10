51e701cb31dcf4a4004c3ca97edec916ad2af6fd
JOHNSONMATTHEY\FernaI01 <ivan.fernandes@matthey.com>
2019-05-10 15:45:19 +0100

    updated for both live and uat

--- StandingDataSQL/user_const_repository_Add_EMIR_FTP.sql
+++ StandingDataSQL/user_const_repository_Add_EMIR_FTP.sql
@@ -4,21 +4,38 @@ select count(*) from USER_const_repository
 
 go
 
-
+-- Log Location
+-- uat
+insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','WinSCPLogLocation',2,'\\gbromeolfs01d\endur_dev\Dirs\SUPPORT\Outdir\\Logs' )
 -- LIVE
 -- insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','WinSCPLogLocation',2,'\\gbromeolfs01p\endur_prod\Dirs\OLEME00P.ENDUR\outdir\Logs' )
 go
 
+-- User
+-- uat
 insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_User',2,'rfrp7048' )
+-- LIVE
+--insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_User',2,'rprp7048' )
 
+-- IP Address
 go
-
+-- uat
 insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_IP',2,'193.110.154.16' )
+-- LIVE
+-- insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_IP',2,'193.110.154.17' )
 
 go
 
-insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_folder',2,'\\gbromeolfs01d\endur_dev\Dirs\SUPPORT\Outdir\reports\EMIR' )
+-- IP port
+-- uat
+insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_port',2,'22' )
+-- LIVE
+-- insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_port',2,'55222' )
 
+
+-- EMIR_folder
+-- uat
+insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_folder',2,'\\gbromeolfs01d\endur_dev\Dirs\SUPPORT\Outdir\reports\EMIR' )
 -- LIVE 
 -- insert into dbo.USER_const_repository([context],[sub_context],[name],[type],[string_value]) values ('Reports','EMIR','EMIR_folder',2,'\\gbromeolfs01p\endur_prod\Dirs\OLEME00P.ENDUR\outdir\reports\EMIR' )
 
