d3b36f3c4a0d02285c707cdb9e336eac3a675591
Nitesh.Vishwakarma <Nitesh.Vishwakarma@KWA-Analytics.com>
2019-05-14 21:15:44 +0530

    Amendment in sql

--- Java/MetalTrasfer_JVS/src/com/olf/jm/metalstransfer/trigger/StampDealsInUserTables.java
+++ Java/MetalTrasfer_JVS/src/com/olf/jm/metalstransfer/trigger/StampDealsInUserTables.java
@@ -11,24 +11,60 @@ public class StampDealsInUserTables implements IScript {
 
 	public void execute(IContainerContext context) throws OException {
 		Table DealstoProcess = Util.NULL_TABLE;
+		Table cancelledDeals = Util.NULL_TABLE;
 		
 		try {
 			Utils.initialiseLog(Constants.Stamp_LOG_FILE);
 			ODateTime extractDateTime;
 			extractDateTime = ODateTime.getServerCurrentDateTime();
 			PluginLog.info("Fetching Strategy deal created on "	+ extractDateTime);	
-			DealstoProcess = fetchdeals();
-			PluginLog.info(DealstoProcess.getNumRows()+" will be stamped in USER_strategy_deals");
+			DealstoProcess = fetchNewdeals();
 			formatTable(DealstoProcess, extractDateTime);
+			PluginLog.info(DealstoProcess.getNumRows()+" will be stamped in USER_strategy_deals");
+			cancelledDeals = fetchCancelleddeals();
+			formatTable(cancelledDeals, extractDateTime);
+			PluginLog.info(cancelledDeals.getNumRows()+" will be stamped in USER_strategy_deals");
+			
 			PluginLog.info("User table updated with strategy deals");				
 		} catch (OException oe) {
 			PluginLog.error("DBUserTable.saveUserTable() failed"+ oe.getMessage());
 			throw oe;
+			
 		} finally {
 			if (Table.isTableValid(DealstoProcess) == 1) {
 				DealstoProcess.destroy();
+				if (Table.isTableValid(cancelledDeals) == 1) {
+					cancelledDeals.destroy();
+			}
 			}
 		}
+		
+	}
+ 
+	protected Table fetchCancelleddeals() throws OException{
+		Table tbldata1 = Util.NULL_TABLE;
+		try{
+			String sqlQuery = "SELECT * FROM (SELECT ab.deal_tracking_num as deal_num,ab.tran_num,ab.tran_status,ab.version_number  FROM ab_tran ab\n" +
+							  " WHERE ab.tran_type = "+ TRAN_TYPE_ENUM.TRAN_TYPE_TRADING_STRATEGY.toInt() + "\n" + 
+							  "   AND ab.ins_type = " + INS_TYPE_ENUM.strategy.toInt() + "\n" +
+							  "   AND ab.toolset = "  + TOOLSET_ENUM.COMPOSER_TOOLSET.toInt() + "\n" +
+							  "   AND ab.tran_status in ("+ TRAN_STATUS_ENUM.TRAN_STATUS_CANCELLED.toInt()+ ")\n" +
+							  "   AND ab.Current_flag = 1)tbl1 \n" +
+							  " EXCEPT SELECT * FROM (SELECT deal_num,tran_num,tran_status,version_number FROM USER_strategy_deals)tbl2";
+					
+			tbldata1 = Table.tableNew("USER_strategy_deals");
+			PluginLog.info("Fetching Strategy deals for stamping in User table USER_strategy_deals");
+			// ALL strategy deals which are not stamped in User table with trans_status NEW and Cancelled
+			
+			int ret = DBaseTable.execISql(tbldata1, sqlQuery);
+			if (ret != OLF_RETURN_CODE.OLF_RETURN_SUCCEED.toInt()) {
+				PluginLog.warn(DBUserTable.dbRetrieveErrorInfo(ret, "Failed to save in  User table USER_strategy_deals "));
+			}
+		}catch (OException oe) {
+			PluginLog.error("DBUserTable  USER_strategy_deals failed" + oe.getMessage());
+			throw oe;
+		}
+		return tbldata1;
 	}
 //Add columns to user table
 protected Table formatTable(Table DealstoProcess,ODateTime extractDateTime)throws OException{
@@ -47,16 +83,16 @@ protected Table formatTable(Table DealstoProcess,ODateTime extractDateTime)throw
 }
 	
 // Fetch Deals to be stamped
-	protected Table fetchdeals() throws OException{
+	protected Table fetchNewdeals() throws OException{
 		Table tbldata = Util.NULL_TABLE;
 		try{
 			String sqlQuery = "SELECT ab.deal_tracking_num as deal_num,ab.tran_num,ab.tran_status,ab.version_number  FROM ab_tran ab\n" +
-							  " WHERE ab.tran_type = "+ TRAN_TYPE_ENUM.TRAN_TYPE_TRADING_STRATEGY.toInt() + "\n" + 
-							  "   AND ab.ins_type = " + INS_TYPE_ENUM.strategy.toInt() + "\n" +
-							  "   AND ab.toolset = "  + TOOLSET_ENUM.COMPOSER_TOOLSET.toInt() + "\n" +
-							  "   AND ab.tran_status in ("+ TRAN_STATUS_ENUM.TRAN_STATUS_NEW.toInt()+ ","+ TRAN_STATUS_ENUM.TRAN_STATUS_CANCELLED.toInt()+ ")\n" +
-							  "   AND ab.Current_flag = 1 \n" +
-							  " AND ab.tran_num not in (select tran_num from USER_strategy_deals)";
+					  " WHERE ab.tran_type = "+ TRAN_TYPE_ENUM.TRAN_TYPE_TRADING_STRATEGY.toInt() + "\n" + 
+					  "   AND ab.ins_type = " + INS_TYPE_ENUM.strategy.toInt() + "\n" +
+					  "   AND ab.toolset = "  + TOOLSET_ENUM.COMPOSER_TOOLSET.toInt() + "\n" +
+					  "   AND ab.tran_status in ("+ TRAN_STATUS_ENUM.TRAN_STATUS_NEW.toInt()+ ","+ TRAN_STATUS_ENUM.TRAN_STATUS_CANCELLED.toInt()+ ")\n" +
+					  "   AND ab.Current_flag = 1 \n" +
+					  " AND ab.tran_num not in (select tran_num from USER_strategy_deals)";
 					
 			tbldata = Table.tableNew("USER_strategy_deals");
 			PluginLog.info("Fetching Strategy deals for stamping in User table USER_strategy_deals");
