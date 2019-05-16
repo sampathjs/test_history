9d7b3d2ac416cbc1901debfc78c77b2282cc5b65
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2019-05-16 12:10:41 +0100

    Corrected the formatting of these files

--- ReportBuilder/DS_CNY_BU_LE_Map.sql
+++ ReportBuilder/DS_CNY_BU_LE_Map.sql
@@ -1,10 +1,5 @@
 --tabletitle BU_LE_relation "Business unit to Legal entity mapping"
 
-SELECT pr.business_unit_id, 
-       pr.legal_entity_id, 
-       p.long_name, 
-       p.short_name 
-FROM   party_relationship pr 
-       JOIN party p 
-         ON pr.legal_entity_id = p.party_id 
-            AND p.party_status = 1 
+SELECT pr.business_unit_id, pr.legal_entity_id, p.long_name, p.short_name 
+	FROM   party_relationship pr 
+	JOIN party p ON (pr.legal_entity_id = p.party_id AND p.party_status = 1)
