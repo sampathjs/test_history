2464017d4d2669460c1104ce31f52a3654038345
Pramod.Garg <pramod.garg@kwa-analytics.com>
2019-07-22 14:57:48 +0530

    Modified BusinessUnitPickListFilter.java, changes has been made to include error pop up for user whenever an account has been selected where associated business unit to account is not autorized.

--- Java/MetalsTransfer/src/com/olf/jm/metalstransfer/field/picklist/BusinessUnitPickListFilter.java
+++ Java/MetalsTransfer/src/com/olf/jm/metalstransfer/field/picklist/BusinessUnitPickListFilter.java
@@ -2,8 +2,11 @@ package com.olf.jm.metalstransfer.field.picklist;
 
 import com.olf.embedded.application.EnumScriptCategory;
 import com.olf.embedded.application.ScriptCategory;
+import com.olf.jm.logging.Logging;
+import com.olf.openjvs.OException;
 import com.olf.openrisk.application.Session;
 import com.olf.openrisk.staticdata.ReferenceChoices;
+import com.olf.openrisk.table.Table;
 import com.olf.openrisk.trading.Field;
 import com.olf.openrisk.trading.Transaction;
 
@@ -18,15 +21,39 @@ import com.olf.openrisk.trading.Transaction;
  * | Rev | Date        | Change Id     | Author          | Description                                                                     |
  * -----------------------------------------------------------------------------------------------------------------------------------------
  * | 001 | 22-Oct-2015 |               | G. Moore        | Initial version.                                                                |
- * | 002 | 12-Nov-2015 |               | G. Moore        | Now extends abstract class.                                                     |
+ * | 002 | 12-Nov-2015 |               | G. Moore        | Now extends abstract class. 
+ * | 003 | 11-Jul-2019 |			   | Pramod Garg     | Restrict BU to only active ones and notification for user to take action                                                   |
  * -----------------------------------------------------------------------------------------------------------------------------------------
  */
 @ScriptCategory({ EnumScriptCategory.EventNotification })
 public class BusinessUnitPickListFilter extends AbstractPickListFilter {
 
-    public ReferenceChoices retrieveOptions(Session session, String accountName) {
-        return super.retrieveOptions(session, accountName);
-    }
+	public ReferenceChoices retrieveOptions(Session session, String accountName)  
+	{
+		ReferenceChoices choices = session.getStaticDataFactory()
+				.createReferenceChoices();
+		try (Table availableChoices = session.getIOFactory().runSQL(
+				getChoicesSql(accountName))) {
+			
+			int count = availableChoices.getRowCount();
+
+			choices = session.getStaticDataFactory().createReferenceChoices(
+					availableChoices, "");
+			if (count <= 0 && !accountName.isEmpty()
+					&& com.olf.openjvs.Util.canAccessGui() == 1) {
+				com.olf.openjvs.Ask
+						.ok("Associated business unit to the selected account " +accountName+ " is not authorized."
+							+ "Please Check the business unit");
+			}
+		} catch (OException e) {
+			Logging.error("Process to populate business unit has failed ", e);
+
+		}
+
+		return choices;
+	}	
+    	
+    
 
     @Override
     String[] getSqlParameters(Session session, Field field, Transaction tran) {
@@ -49,7 +76,7 @@ public class BusinessUnitPickListFilter extends AbstractPickListFilter {
                 "\n   FROM party pty" +
                 "\n   JOIN party_account pa ON (pa.party_id = pty.party_id)" +
                 "\n   JOIN account acc ON (acc.account_id = pa.account_id)" +
-                "\n  WHERE acc.account_name = '" + params[0] + "'";
+                "\n  WHERE pty.party_status = 1 AND acc.account_name = '" + params[0] + "'";
     }
 }
 
