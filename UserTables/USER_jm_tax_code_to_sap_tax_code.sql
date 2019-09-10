6e9adf134445eaf0798afe92e1a6979ee5255e68
JOHNSONMATTHEY\StoevD01 <dimitre.stoev@matthey.com>
2019-09-10 14:36:39 +0100

    Kala

--- UserTables/USER_jm_tax_code_to_sap_tax_code.sql
+++ UserTables/USER_jm_tax_code_to_sap_tax_code.sql
@@ -1,11 +1,3 @@
-------------------------------------------------------------------------------
--- sap tax code mapping table
-------------------------------------------------------------------------------
-IF OBJECT_ID('dbo.USER_jm_tax_code_to_sap_tax_code' , 'U') IS NOT NULL
-	  DROP TABLE [dbo].[USER_jm_tax_code_to_sap_tax_code] 
-
-GO
-
 create table USER_jm_tax_code_to_sap_tax_code
 (
 tax_type varchar(255),
@@ -13,19 +5,11 @@ tax_subtype varchar(255),
 instrument_types varchar(255),
 buy_sell varchar(255),
 ext_bu varchar(255),
-o_tax_code varchar(255)
+o_tax_code varchar(255),
+int_bu varchar(255)
 )
 
-grant select, insert, update, delete on USER_jm_tax_code_to_sap_tax_code to olf_user,olf_user_manual
+grant select, insert, update, delete on USER_jm_tax_code_to_sap_tax_code to olf_user
 
 grant select on USER_jm_tax_code_to_sap_tax_code to olf_readonly
-
-
-
-
-select * from USER_jm_tax_code_to_sap_tax_code
- 
-select COUNT(*) from USER_jm_tax_code_to_sap_tax_code
- 
-
 
