7e2a9cf59427ef8f9d019d96cb704fbc55128bab
Nitesh.Vishwakarma <Nitesh.Vishwakarma@KWA-Analytics.com>
2019-09-17 19:51:19 +0530

    Update Constants.java

--- Java/MetalTransfer_JVS/src/com/olf/jm/metalstransfer/utils/Constants.java
+++ Java/MetalTransfer_JVS/src/com/olf/jm/metalstransfer/utils/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
 	public static final String LOG_FILE_NAME = "TriggerMetalTransfer.log";
 	public static final String LOG_FILE = "CancelMetalTransfer.log";
 	public static final String Stamp_LOG_FILE = "StampDealsInUserTables.log";
-	public static String ALERTMAILLOG = "AlertForAssignments.log";
+	public static final String ALERTMAILLOG = "AlertForAssignments.log";
+	public static final String ALERTQUOTES = "AlertForQuoteTransfers.log";
+	
 
 }
