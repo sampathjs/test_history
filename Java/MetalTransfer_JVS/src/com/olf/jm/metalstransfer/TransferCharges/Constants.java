257c9db23a54cf0cea16d79610c35801adb6a12d
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2019-09-18 17:33:16 +0100

    Removed

--- Java/MetalTransfer_JVS/src/com/olf/jm/metalstransfer/TransferCharges/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.olf.jm.metalstransfer.transfercharges;
-
-public class Constants {
-
-	public static final String LOG_FILE_NAME = "TransferChargesTriggerScript.log";
-	public static final String wflow_name = "TriggerMetalTransferCharges";
-}
