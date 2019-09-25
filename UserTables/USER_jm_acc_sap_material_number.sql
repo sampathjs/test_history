2ce3da2fa8960fb6181560f925d64069d63d8f99
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2019-09-25 16:59:26 +0100

    added latest SQL

--- UserTables/USER_jm_acc_sap_material_number.sql
+++ UserTables/USER_jm_acc_sap_material_number.sql
@@ -3,14 +3,16 @@ IF OBJECT_ID('dbo.USER_jm_acc_sap_material_number' , 'U') IS NOT NULL
 GO
 ----
 
-create table 	
+create table USER_jm_acc_sap_material_number
 (
 currency varchar(255),
 form varchar(255),
 uom varchar(255),
 o_sap_uom varchar(255),
-o_sap_material_number varchar(255)
+o_sap_material_number varchar(255),
+int_bu varchar(255)
 )
+
 
 grant select, insert, update, delete on USER_jm_acc_sap_material_number to olf_user
 grant select on USER_jm_acc_sap_material_number to olf_readonly
--- UserTables/USER_jm_tax_code_to_sap_tax_code.sql
+++ UserTables/USER_jm_tax_code_to_sap_tax_code.sql
@@ -8,10 +8,14 @@ create table USER_jm_tax_code_to_sap_tax_code
 tax_type varchar(255),
 tax_subtype varchar(255),
 instrument_types varchar(255),
+cflow_type varchar(255),
 buy_sell varchar(255),
+int_bu varchar(255),
 ext_bu varchar(255),
+cpy_is_jm_group varchar(255),
 o_tax_code varchar(255)
 )
+
 
 grant select, insert, update, delete on USER_jm_tax_code_to_sap_tax_code to olf_user
 grant select on USER_jm_tax_code_to_sap_tax_code to olf_readonly
