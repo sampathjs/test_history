57c8742424cd8f121869878c0dfcf7dabdaf3fff
JOHNSONMATTHEY\JoshiG01 <Giriraj.Joshi@matthey.com>
2019-11-12 12:59:52 +0000

    EUP-11. Changed code as structure of return table is changed in v17. Kept the code compatible to v14.

--- Java/CustomPickListsJVS/src/com/olf/jm/custom_picklists/app/NonPreciousMetalCurrencyPickList.java
+++ Java/CustomPickListsJVS/src/com/olf/jm/custom_picklists/app/NonPreciousMetalCurrencyPickList.java
@@ -19,13 +19,28 @@ public class NonPreciousMetalCurrencyPickList implements IScript {
 	{
 		String sql =
 				"SELECT id_number, name FROM currency WHERE precious_metal = 0";
-		Table retValues = context.getReturnTable().getTable("table_value", 1);
+		
+		Table retValues = context.getReturnTable();
+		
+		/*** v17 change - Structure of return table has changed. Added check below. ***/
+		boolean v17Struct = context.getReturnTable().getColName(1).equalsIgnoreCase("table_value") ? false : true;
+		if (!v17Struct) {
+			retValues = retValues.getTable("table_value", 1);
+		} 
+		
 		Table sqlResult = Table.tableNew("sql_result");
 		DBaseTable.execISql(sqlResult, sql);
 		retValues.addCol("delete", COL_TYPE_ENUM.COL_INT);
 		sqlResult.addCol("delete", COL_TYPE_ENUM.COL_INT);
 		retValues.setColValInt("delete", 1);
-		retValues.select(sqlResult, "delete", "id_number EQ $id");
+		
+		/*** v17 change - Structure of return table has changed. Added check below. ***/
+		if (v17Struct) {
+			retValues.select(sqlResult, "delete", "id_number EQ $value");
+		} else {
+			retValues.select(sqlResult, "delete", "id_number EQ $id_number");
+		} 
+		
 		retValues.deleteWhereValue("delete", 1);
 		retValues.delCol("delete");
 	}	
