e82ca5526a025fec8352a4f1af163855d0a6e024
JOHNSONMATTHEY\FernaI01 <ivan.fernandes@matthey.com>
2019-11-12 10:31:48 +0000

    formatting changes

--- ReportBuilder/DS_wavg_delivery_price.sql
+++ ReportBuilder/DS_wavg_delivery_price.sql
@@ -1,28 +1,22 @@
 -- SWAP DEALS NON-UNIFY
-SELECT
-	u.deal_num
-	,sum(u.delivery_price*u.delivery_volume)/sum(u.delivery_volume) as wavg_delivery_price
-FROM 
-	USER_jm_trading_pnl_history u 
-	INNER JOIN ab_tran ab on u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1
-	INNER JOIN parameter pm on ab.ins_num = pm.ins_num AND pm.param_seq_num = u.deal_leg
-	INNER JOIN ab_tran abt on ab.template_tran_num = abt.tran_num AND abt.current_flag = 1
-WHERE
-	ab.toolset = 15
-	AND pm.fx_flt = 1
+SELECT 	u.deal_num, SUM(u.delivery_price*u.delivery_volume)/SUM(u.delivery_volume) AS wavg_delivery_price
+    FROM USER_jm_trading_pnl_history u 
+	INNER JOIN ab_tran ab ON (u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1)
+	INNER JOIN parameter pm ON (ab.ins_num = pm.ins_num AND pm.param_seq_num = u.deal_leg)
+	INNER JOIN ab_tran abt ON (ab.template_tran_num = abt.tran_num AND abt.current_flag = 1)
+WHERE ab.toolset = 15	-- Swap
+	AND pm.fx_flt = 1	-- Float
 	AND abt.reference != 'Coverage_Benchmarks'
-GROUP BY
-	deal_num
+GROUP BY deal_num
 -- SWAP DEALS UNIFY (BUG IN SAVING CORRECT DEAL_LEG TO PNL_HISTORY NEEDS ALTERNATIVE JOIN CONDITIONS
 UNION
 SELECT
-	u.deal_num
-	,sum(u.delivery_price*u.delivery_volume)/sum(u.delivery_volume) as wavg_delivery_price
+	u.deal_num,SUM(u.delivery_price*u.delivery_volume)/SUM(u.delivery_volume) AS wavg_delivery_price
 FROM 
 	USER_jm_trading_pnl_history u 
-	INNER JOIN ab_tran ab on u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1
-	INNER JOIN ab_tran abt on ab.template_tran_num = abt.tran_num AND abt.current_flag = 1
-	INNER JOIN currency ccy on ccy.id_number = u.metal_ccy
+	INNER JOIN ab_tran ab ON (u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1)
+	INNER JOIN ab_tran abt ON (ab.template_tran_num = abt.tran_num AND abt.current_flag = 1)
+	INNER JOIN currency ccy ON (ccy.id_number = u.metal_ccy)
 WHERE
 	ab.toolset = 15
 	AND abt.reference = 'Coverage_Benchmarks'
@@ -33,13 +27,13 @@ GROUP BY
 UNION 
 SELECT
 	u.deal_num
-	,sum(u.delivery_price*u.delivery_volume)/sum(u.delivery_volume) as wavg_delivery_price
+	,SUM(u.delivery_price*u.delivery_volume)/SUM(u.delivery_volume) AS wavg_delivery_price
 FROM 
 	USER_jm_trading_pnl_history u 
-	INNER JOIN ab_tran ab on u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1
-	INNER JOIN parameter pm on ab.ins_num = pm.ins_num AND pm.param_seq_num = u.deal_leg 
+	INNER JOIN ab_tran ab ON (u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1)
+	INNER JOIN parameter pm ON (ab.ins_num = pm.ins_num AND pm.param_seq_num = u.deal_leg)
 WHERE
-	ab.toolset != 15
+	ab.toolset != 15 -- Swap
 	AND pm.param_seq_num = 0
 GROUP BY
 	deal_num
@@ -47,25 +41,24 @@ GROUP BY
 UNION
 SELECT
 	u.deal_num
-	,sum(u.delivery_price*u.delivery_volume)/sum(u.delivery_volume) as wavg_delivery_price
+	,SUM(u.delivery_price*u.delivery_volume)/SUM(u.delivery_volume) AS wavg_delivery_price
 FROM 
 	USER_jm_trading_pnl_history_cn u 
-	INNER JOIN ab_tran ab on u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1
+	INNER JOIN ab_tran ab ON (u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1)
 WHERE
-	ab.toolset = 15
+	ab.toolset = 15 -- Swap
 GROUP BY
 	deal_num
 -- CN NON SWAP DEALS
 UNION 
 SELECT
 	u.deal_num
-	,sum(u.delivery_price*u.delivery_volume)/sum(u.delivery_volume) as wavg_delivery_price
+	,SUM(u.delivery_price*u.delivery_volume)/SUM(u.delivery_volume) AS wavg_delivery_price
 FROM 
 	USER_jm_trading_pnl_history_cn u 
-	INNER JOIN ab_tran ab on u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1
-	INNER JOIN parameter pm on ab.ins_num = pm.ins_num AND pm.param_seq_num = u.deal_leg 
+	INNER JOIN ab_tran ab ON (u.deal_num = ab.deal_tracking_num AND ab.current_flag = 1)
+	INNER JOIN parameter pm ON (ab.ins_num = pm.ins_num AND pm.param_seq_num = u.deal_leg)
 WHERE
-	ab.toolset != 15
+	ab.toolset != 15 -- Swap
 	AND pm.param_seq_num = 0
-GROUP BY
-deal_num
+GROUP BY deal_num
