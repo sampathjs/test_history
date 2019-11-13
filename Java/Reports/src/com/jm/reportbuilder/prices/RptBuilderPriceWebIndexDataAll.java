2deabea49d852f582debdb7422c48c1a0ddf1a4a
JOHNSONMATTHEY\FernaI01 <ivan.fernandes@matthey.com>
2019-11-13 14:50:26 +0000

    changes from code review

--- Java/Reports/src/com/jm/reportbuilder/prices/RptBuilderPriceWebIndexDataAll.java
+++ Java/Reports/src/com/jm/reportbuilder/prices/RptBuilderPriceWebIndexDataAll.java
@@ -1,13 +1,10 @@
 package com.jm.reportbuilder.prices;
 
-
-
 import com.olf.openjvs.DBaseTable;
 import com.olf.openjvs.IContainerContext;
 import com.olf.openjvs.IScript;
 import com.olf.openjvs.OCalendar;
 import com.olf.openjvs.OConsole;
-import com.olf.openjvs.ODateTime;
 import com.olf.openjvs.OException;
 import com.olf.openjvs.Ref;
 import com.olf.openjvs.SystemUtil;
@@ -15,8 +12,6 @@ import com.olf.openjvs.Table;
 import com.olf.openjvs.Util;
 import com.olf.openjvs.enums.COL_TYPE_ENUM;
 import com.olf.openjvs.enums.SEARCH_CASE_ENUM;
-//import com.olf.openjvs.enums.COL_TYPE_ENUM;
-//import com.olf.openjvs.enums.OLF_RETURN_CODE;
 import com.olf.openjvs.enums.SHM_USR_TABLES_ENUM;
 import com.openlink.util.constrepository.ConstRepository;
 import com.openlink.util.logging.PluginLog;
@@ -38,122 +33,146 @@ public class RptBuilderPriceWebIndexDataAll implements IScript{
 			
 			Table tblParam = tblArgt.getTable("PluginParameters", 1);
 			
-			int intRowNum = 0;
+			int intModeFlag = tblArgt.getInt("ModeFlag",1);
 			
-			intRowNum = tblParam.unsortedFindString("parameter_name", "RefSource", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strRefSrc = tblParam.getString("parameter_value", intRowNum);
-
-			String strRefSrcSQL = "";
-
 			
-			String strRefSrcArray []  = strRefSrc.split(",");
-			
-			for(int i=0;i<strRefSrcArray.length;i++){
+			if (intModeFlag == 0)
+			{
 				
-				if(i==0 || i == strRefSrcArray.length){
-					
-					strRefSrcSQL += "'" +  Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, strRefSrcArray[i]) + "'";
-				}else{
-					strRefSrcSQL += ",'" + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE,strRefSrcArray[i]) + "'";
-				}
-			}
-			
-			intRowNum = tblParam.unsortedFindString("parameter_name", "Metal", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strMetal = tblParam.getString("parameter_value", intRowNum);
+				Table returnt = context.getReturnTable();
 
-			String strMetalSQL = "";
-
-			String strMetalArray []  = strMetal.split(",");
-
-			for(int i=0;i<strMetalArray.length;i++){
+				returnt.addCol("ref_source", COL_TYPE_ENUM.COL_STRING);
+				returnt.addCol("reset_date", COL_TYPE_ENUM.COL_DATE_TIME);
+				returnt.addCol("xag", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("xau", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("xir", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("xos", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("xpd", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("xpt", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("xrh", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("xru", COL_TYPE_ENUM.COL_DOUBLE);
 				
-				if(i==0 || i == strMetalArray.length){
+			}			
+			else{
+				int intRowNum = 0;
+				
+				intRowNum = tblParam.unsortedFindString("parameter_name", "RefSource", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strRefSrc = tblParam.getString("parameter_value", intRowNum);
+
+				String strRefSrcSQL = "";
+
+				
+				String strRefSrcArray []  = strRefSrc.split(",");
+				
+				for(int i=0;i<strRefSrcArray.length;i++){
 					
-					strMetalSQL += "'" +  strMetalArray[i] + "'";
-				}else{
-					strMetalSQL += ",'" + strMetalArray[i] + "'";
+					if(i==0 || i == strRefSrcArray.length){
+						
+						strRefSrcSQL += "'" +  Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, strRefSrcArray[i]) + "'";
+					}else{
+						strRefSrcSQL += ",'" + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE,strRefSrcArray[i]) + "'";
+					}
 				}
+				
+				intRowNum = tblParam.unsortedFindString("parameter_name", "Metal", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strMetal = tblParam.getString("parameter_value", intRowNum);
+
+				String strMetalSQL = "";
+
+				String strMetalArray []  = strMetal.split(",");
+
+				for(int i=0;i<strMetalArray.length;i++){
+					
+					if(i==0 || i == strMetalArray.length){
+						
+						strMetalSQL += "'" +  strMetalArray[i] + "'";
+					}else{
+						strMetalSQL += ",'" + strMetalArray[i] + "'";
+					}
+				}
+				
+				intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateStart", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strResetDateStartJD = tblParam.getString("parameter_value", intRowNum);
+
+				intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateEnd", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strResetDateEndJD = tblParam.getString("parameter_value", intRowNum);
+
+				
+				Table returnt = context.getReturnTable();
+				
+				String strSQL;
+				strSQL = "SELECT \n";
+				strSQL += " replace(idx.index_name,'.USD','') as metal\n";
+				strSQL += ",ihp.reset_date \n";
+				strSQL += ",ihp.price \n";
+				strSQL += ",case when (rs.name = 'LBMA AM' or rs.name = 'LME AM') then 'London AM' \n";
+				strSQL += "when (rs.name = 'LBMA PM' or rs.name = 'LME PM' or rs.name = 'LBMA Silver') then 'London PM' \n";
+				strSQL += "else rs.name \n";
+				strSQL += "end as ref_source \n";
+				strSQL += "FROM \n";
+				strSQL += "idx_historical_prices ihp\n";
+				strSQL += "inner join idx_def idx on idx.index_id =ihp.index_id and idx.db_status = 1 and idx.currency = 0 \n";
+				strSQL += "inner join ref_source rs on rs.id_number = ihp.ref_source \n";
+				strSQL += "WHERE \n";
+				strSQL += "ihp.ref_source in (" + strRefSrcSQL + ") \n"; 
+				strSQL += "AND ihp.reset_date >= " + OCalendar.parseString(strResetDateStartJD) + " \n";
+				strSQL += "AND ihp.reset_date <= " + OCalendar.parseString(strResetDateEndJD) + " \n";
+				strSQL += "AND replace(idx.index_name,'.USD','') in (" +  strMetalSQL + ")";
+
+				Table tblPrices = Table.tableNew();
+				DBaseTable.execISql(tblPrices, strSQL);
+
+				tblPivot = tblPrices.pivot("metal", "reset_date,ref_source", "price", " ");
+				
+				// validate table structure
+				if(tblPivot.getColNum("XPD") < 0){
+					tblPivot.addCol("XPD", COL_TYPE_ENUM.COL_DOUBLE);
+					tblPivot.setColValDouble("XPD", 0.0);
+				}
+				
+
+				if(tblPivot.getColNum("XPT") < 0){
+					tblPivot.addCol("XPT", COL_TYPE_ENUM.COL_DOUBLE);
+					tblPivot.setColValDouble("XPT", 0.0);
+				}
+
+				if(tblPivot.getColNum("XRH") < 0){
+					tblPivot.addCol("XRH", COL_TYPE_ENUM.COL_DOUBLE);
+					tblPivot.setColValDouble("XRH", 0.0);
+				}
+
+				if(tblPivot.getColNum("XRU") < 0){
+					tblPivot.addCol("XRU", COL_TYPE_ENUM.COL_DOUBLE);
+					tblPivot.setColValDouble("XRU", 0.0); 
+				}
+
+				if(tblPivot.getColNum("XIR") < 0){
+					tblPivot.addCol("XIR", COL_TYPE_ENUM.COL_DOUBLE);
+					tblPivot.setColValDouble("XIR", 0.0);
+				}
+
+
+				if(tblPivot.getColNum("XAU") < 0){
+					tblPivot.addCol("XAU", COL_TYPE_ENUM.COL_DOUBLE);
+					tblPivot.setColValDouble("XAU", 0.0);
+				}
+
+				
+				if(tblPivot.getColNum("XAG") < 0){
+					tblPivot.addCol("XAG", COL_TYPE_ENUM.COL_DOUBLE);
+					tblPivot.setColValDouble("XAG", 0.0);
+				}
+
+
+				returnt.select(tblPivot, "*", "ref_source NE ''");
+
+				
+				
+				tblPrices.destroy();
+
+				
 			}
-			
-			intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateStart", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strResetDateStartJD = tblParam.getString("parameter_value", intRowNum);
-
-			intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateEnd", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strResetDateEndJD = tblParam.getString("parameter_value", intRowNum);
-
-			
-			Table returnt = context.getReturnTable();
-			
-			String strSQL;
-			strSQL = "SELECT \n";
-			strSQL += " replace(idx.index_name,'.USD','') as metal\n";
-			strSQL += ",ihp.reset_date \n";
-			strSQL += ",ihp.price \n";
-			strSQL += ",case when (rs.name = 'LBMA AM' or rs.name = 'LME AM') then 'London AM' \n";
-			strSQL += "when (rs.name = 'LBMA PM' or rs.name = 'LME PM' or rs.name = 'LBMA Silver') then 'London PM' \n";
-			strSQL += "else rs.name \n";
-			strSQL += "end as ref_source \n";
-			strSQL += "FROM \n";
-			strSQL += "idx_historical_prices ihp\n";
-			strSQL += "inner join idx_def idx on idx.index_id =ihp.index_id and idx.db_status = 1 and idx.currency = 0 \n";
-			strSQL += "inner join ref_source rs on rs.id_number = ihp.ref_source \n";
-			strSQL += "WHERE \n";
-			strSQL += "ihp.ref_source in (" + strRefSrcSQL + ") \n"; 
-			strSQL += "AND ihp.reset_date >= " + OCalendar.parseString(strResetDateStartJD) + " \n";
-			strSQL += "AND ihp.reset_date <= " + OCalendar.parseString(strResetDateEndJD) + " \n";
-			strSQL += "AND replace(idx.index_name,'.USD','') in (" +  strMetalSQL + ")";
-
-			Table tblPrices = Table.tableNew();
-			DBaseTable.execISql(tblPrices, strSQL);
-
-			tblPivot = tblPrices.pivot("metal", "reset_date,ref_source", "price", " ");
-			
-			// validate table structure
-			if(tblPivot.getColNum("XPD") < 0){
-				tblPivot.addCol("XPD", COL_TYPE_ENUM.COL_DOUBLE);
-				tblPivot.setColValDouble("XPD", 0.0);
-			}
-			
-
-			if(tblPivot.getColNum("XPT") < 0){
-				tblPivot.addCol("XPT", COL_TYPE_ENUM.COL_DOUBLE);
-				tblPivot.setColValDouble("XPT", 0.0);
-			}
-
-			if(tblPivot.getColNum("XRH") < 0){
-				tblPivot.addCol("XRH", COL_TYPE_ENUM.COL_DOUBLE);
-				tblPivot.setColValDouble("XRH", 0.0);
-			}
-
-			if(tblPivot.getColNum("XRU") < 0){
-				tblPivot.addCol("XRU", COL_TYPE_ENUM.COL_DOUBLE);
-				tblPivot.setColValDouble("XRU", 0.0); 
-			}
-
-			if(tblPivot.getColNum("XIR") < 0){
-				tblPivot.addCol("XIR", COL_TYPE_ENUM.COL_DOUBLE);
-				tblPivot.setColValDouble("XIR", 0.0);
-			}
-
-
-			if(tblPivot.getColNum("XAU") < 0){
-				tblPivot.addCol("XAU", COL_TYPE_ENUM.COL_DOUBLE);
-				tblPivot.setColValDouble("XAU", 0.0);
-			}
-
-			
-			if(tblPivot.getColNum("XAG") < 0){
-				tblPivot.addCol("XAG", COL_TYPE_ENUM.COL_DOUBLE);
-				tblPivot.setColValDouble("XAG", 0.0);
-			}
-
-
-			returnt.select(tblPivot, "*", "ref_source NE ''");
-
-			
-			
-			tblPrices.destroy();
-			
+					
 		} catch (Throwable ex) {
 			OConsole.oprint(ex.toString());
 			PluginLog.error(ex.toString());
--- Java/Reports/src/com/jm/reportbuilder/prices/RptBuilderPriceWebIndexDataAllByRange.java
+++ Java/Reports/src/com/jm/reportbuilder/prices/RptBuilderPriceWebIndexDataAllByRange.java
@@ -7,7 +7,6 @@ import com.olf.openjvs.IContainerContext;
 import com.olf.openjvs.IScript;
 import com.olf.openjvs.OCalendar;
 import com.olf.openjvs.OConsole;
-import com.olf.openjvs.ODateTime;
 import com.olf.openjvs.OException;
 import com.olf.openjvs.Ref;
 import com.olf.openjvs.SystemUtil;
@@ -35,157 +34,173 @@ public class RptBuilderPriceWebIndexDataAllByRange implements IScript{
 			
 			Table tblArgt = context.getArgumentsTable();
 
-			Table tblParam = tblArgt.getTable("PluginParameters", 1);
+			int intModeFlag = tblArgt.getInt("ModeFlag", 1);
 			
 
-			intRowNum = tblParam.unsortedFindString("parameter_name", "RefSource", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strRefSrc = tblParam.getString("parameter_value", intRowNum);
-
-			String strRefSrcSQL = "";
-
-			String strRefSrcArray []  = strRefSrc.split(",");
-			
-			for(int i=0;i<strRefSrcArray.length;i++){
+			if (intModeFlag == 0)
+			{
 				
-				if(i==0 || i == strRefSrcArray.length){
+				Table returnt = context.getReturnTable();
+
+				returnt.addCol("max_mtl_px", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("metal", COL_TYPE_ENUM.COL_STRING);
+				returnt.addCol("min_mtl_px", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("mtl_avg", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("price", COL_TYPE_ENUM.COL_DOUBLE);
+				returnt.addCol("ref_source_id", COL_TYPE_ENUM.COL_INT);
+				
+			}			
+			else{
+				
+				Table tblParam = tblArgt.getTable("PluginParameters", 1);
+				
+
+				intRowNum = tblParam.unsortedFindString("parameter_name", "RefSource", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strRefSrc = tblParam.getString("parameter_value", intRowNum);
+
+				String strRefSrcSQL = "";
+
+				String strRefSrcArray []  = strRefSrc.split(",");
+				
+				for(int i=0;i<strRefSrcArray.length;i++){
 					
-					strRefSrcSQL += "'" +  Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, strRefSrcArray[i]) + "'";
-				}else{
-					strRefSrcSQL += ",'" + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE,strRefSrcArray[i]) + "'";
-				}
-			}
-			
-			intRowNum = tblParam.unsortedFindString("parameter_name", "Metal", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strMetal = tblParam.getString("parameter_value", intRowNum);
-
-			String strMetalSQL = "";
-
-			String strMetalArray []  = strMetal.split(",");
-
-			for(int i=0;i<strMetalArray.length;i++){
-				
-				if(i==0 || i == strMetalArray.length){
-					
-					strMetalSQL += "'" +  strMetalArray[i] + "'";
-				}else{
-					strMetalSQL += ",'" + strMetalArray[i] + "'";
-				}
-			}
-			
-			intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateStart", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strResetDateStartJD = tblParam.getString("parameter_value", intRowNum);
-
-			intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateEnd", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
-			String strResetDateEndJD = tblParam.getString("parameter_value", intRowNum);
-
-			
-			Table returnt = context.getReturnTable();
-			
-			String strSQL;
-			strSQL = "SELECT \n";
-			//strSQL += " replace(idx.index_name,'.USD','') as metal\n";
-			
-		 	strSQL += "case when replace(idx.index_name,'.USD','') = 'XAG' then 'Silver' \n";
-		 	strSQL += "when replace(idx.index_name,'.USD','') = 'XAU' then 'Gold' \n";
-		 	strSQL += "when replace(idx.index_name,'.USD','') = 'XIR' then 'Iridium' \n";
-		 	strSQL += "when replace(idx.index_name,'.USD','') = 'XOS' then 'Osmium' \n";
-		 	strSQL += "when replace(idx.index_name,'.USD','') = 'XPD' then 'Palladium' \n";
-		 	strSQL += "when replace(idx.index_name,'.USD','') = 'XPT' then 'Platinum' \n";
-		 	strSQL += "when replace(idx.index_name,'.USD','') = 'XRH' then 'Rhodium' \n";
-		 	strSQL += "when replace(idx.index_name,'.USD','') = 'XRU' then 'Ruthenium' \n";
-		 	strSQL += "end as metal \n";
-
-			
-			strSQL += ",ihp.reset_date \n";
-			strSQL += ",ihp.price \n";
-			strSQL += ",case when (rs.name = 'LBMA AM' or rs.name = 'LME AM') then " + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, "LME AM")+ " \n";
-			strSQL += "when (rs.name = 'LBMA PM' or rs.name = 'LME PM' or rs.name = 'LBMA Silver') then  " + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, "LME PM")+ " \n";
-			strSQL += "else rs.id_number \n";
-			strSQL += "end as ref_source_id \n";
-
-			
-			strSQL += "FROM \n";
-			strSQL += "idx_historical_prices ihp\n";
-			strSQL += "inner join idx_def idx on idx.index_id =ihp.index_id and idx.db_status = 1 and idx.currency = 0 \n";
-			strSQL += "inner join ref_source rs on rs.id_number = ihp.ref_source \n";
-			strSQL += "WHERE \n";
-			strSQL += "ihp.ref_source in (" + strRefSrcSQL + ") \n"; 
-			strSQL += "AND ihp.reset_date >= " + OCalendar.parseString(strResetDateStartJD) + " \n";
-			strSQL += "AND ihp.reset_date <= " + OCalendar.parseString(strResetDateEndJD) + " \n";
-			strSQL += "AND replace(idx.index_name,'.USD','') in (" +  strMetalSQL + ") \n";
-			strSQL += "ORDER BY ref_source_id, replace(idx.index_name,'.USD',''), ihp.reset_date asc";
-
-			Table tblPrices = Table.tableNew();
-			DBaseTable.execISql(tblPrices, strSQL);
-
-			
-			tblPrices.addCol("mtl_avg",COL_TYPE_ENUM.COL_DOUBLE);
-			
-			tblPrices.addCol("min_mtl_px",COL_TYPE_ENUM.COL_DOUBLE);
-			tblPrices.addCol("max_mtl_px",COL_TYPE_ENUM.COL_DOUBLE);
-			
-			tblPrices.setColFormatAsDouble("mtl_avg", 2, 2);
-			tblPrices.setColFormatAsDouble("min_mtl_px", 2, 2);
-			tblPrices.setColFormatAsDouble("min_mtl_px", 2, 2);
-			
-			Table tblMetal = Table.tableNew();
-			tblMetal.select(tblPrices,"DISTINCT,metal","ref_source_id GT 0");
-			
-			for(int i = 1;i<=tblMetal.getNumRows();i++){
-				
-				Table tblCurrMtl = Table.tableNew();
-				
-				tblCurrMtl.select(tblPrices, "*", "metal EQ " + tblMetal.getString("metal",i) );
-
-				double dblSum = tblCurrMtl.sumRangeDouble("price", 1, tblCurrMtl.getNumRows());
-				double dblNumDays = Double.valueOf(tblCurrMtl.getNumRows()); 
-				double dblAvg = dblSum /dblNumDays;
-				
-				tblCurrMtl.setColValDouble("mtl_avg",dblAvg);
-				
-				tblPrices.select(tblCurrMtl, "mtl_avg", "metal EQ $metal AND reset_date EQ $reset_date AND ref_source_id EQ $ref_source_id " );
-
-				
-				double dblMinPrice = tblCurrMtl.getDouble("price",1);
-				for(int j = 1;j<=tblCurrMtl.getNumRows();j++){
-					
-					if(tblCurrMtl.getDouble("price", j)<dblMinPrice){
-						dblMinPrice = tblCurrMtl.getDouble("price",j);
+					if(i==0 || i == strRefSrcArray.length){
+						
+						strRefSrcSQL += "'" +  Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, strRefSrcArray[i]) + "'";
+					}else{
+						strRefSrcSQL += ",'" + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE,strRefSrcArray[i]) + "'";
 					}
 				}
 				
-				tblCurrMtl.setColValDouble("min_mtl_px",dblMinPrice);
-				
-				tblPrices.select(tblCurrMtl, "min_mtl_px", "metal EQ $metal AND reset_date EQ $reset_date AND ref_source_id EQ $ref_source_id " );
-				
-				
-				double dblMaxPrice = tblCurrMtl.getDouble("price",1);
-				for(int j = 1;j<=tblCurrMtl.getNumRows();j++){
+				intRowNum = tblParam.unsortedFindString("parameter_name", "Metal", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strMetal = tblParam.getString("parameter_value", intRowNum);
+
+				String strMetalSQL = "";
+
+				String strMetalArray []  = strMetal.split(",");
+
+				for(int i=0;i<strMetalArray.length;i++){
 					
-					if(tblCurrMtl.getDouble("price", j)>dblMaxPrice){
-						dblMaxPrice = tblCurrMtl.getDouble("price",j);
+					if(i==0 || i == strMetalArray.length){
+						
+						strMetalSQL += "'" +  strMetalArray[i] + "'";
+					}else{
+						strMetalSQL += ",'" + strMetalArray[i] + "'";
 					}
 				}
+				
+				intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateStart", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strResetDateStartJD = tblParam.getString("parameter_value", intRowNum);
 
-				tblCurrMtl.setColValDouble("max_mtl_px",dblMaxPrice);
-				tblPrices.select(tblCurrMtl, "max_mtl_px", "metal EQ $metal AND reset_date EQ $reset_date AND ref_source_id EQ $ref_source_id " );
+				intRowNum = tblParam.unsortedFindString("parameter_name", "ResetDateEnd", SEARCH_CASE_ENUM.CASE_INSENSITIVE);
+				String strResetDateEndJD = tblParam.getString("parameter_value", intRowNum);
+
+				
+				Table returnt = context.getReturnTable();
+				
+				String strSQL;
+				strSQL = "SELECT \n";
+			 	strSQL += "case when replace(idx.index_name,'.USD','') = 'XAG' then 'Silver' \n";
+			 	strSQL += "when replace(idx.index_name,'.USD','') = 'XAU' then 'Gold' \n";
+			 	strSQL += "when replace(idx.index_name,'.USD','') = 'XIR' then 'Iridium' \n";
+			 	strSQL += "when replace(idx.index_name,'.USD','') = 'XOS' then 'Osmium' \n";
+			 	strSQL += "when replace(idx.index_name,'.USD','') = 'XPD' then 'Palladium' \n";
+			 	strSQL += "when replace(idx.index_name,'.USD','') = 'XPT' then 'Platinum' \n";
+			 	strSQL += "when replace(idx.index_name,'.USD','') = 'XRH' then 'Rhodium' \n";
+			 	strSQL += "when replace(idx.index_name,'.USD','') = 'XRU' then 'Ruthenium' \n";
+			 	strSQL += "end as metal \n";
+				strSQL += ",ihp.reset_date \n";
+				strSQL += ",ihp.price \n";
+				strSQL += ",case when (rs.name = 'LBMA AM' or rs.name = 'LME AM') then " + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, "LME AM")+ " \n";
+				strSQL += "when (rs.name = 'LBMA PM' or rs.name = 'LME PM' or rs.name = 'LBMA Silver') then  " + Ref.getValue(SHM_USR_TABLES_ENUM.REF_SOURCE_TABLE, "LME PM")+ " \n";
+				strSQL += "else rs.id_number \n";
+				strSQL += "end as ref_source_id \n";
+				strSQL += "FROM \n";
+				strSQL += "idx_historical_prices ihp\n";
+				strSQL += "inner join idx_def idx on idx.index_id =ihp.index_id and idx.db_status = 1 and idx.currency = 0 \n";
+				strSQL += "inner join ref_source rs on rs.id_number = ihp.ref_source \n";
+				strSQL += "WHERE \n";
+				strSQL += "ihp.ref_source in (" + strRefSrcSQL + ") \n"; 
+				strSQL += "AND ihp.reset_date >= " + OCalendar.parseString(strResetDateStartJD) + " \n";
+				strSQL += "AND ihp.reset_date <= " + OCalendar.parseString(strResetDateEndJD) + " \n";
+				strSQL += "AND replace(idx.index_name,'.USD','') in (" +  strMetalSQL + ") \n";
+				strSQL += "ORDER BY ref_source_id, replace(idx.index_name,'.USD',''), ihp.reset_date asc";
+
+				Table tblPrices = Table.tableNew();
+				DBaseTable.execISql(tblPrices, strSQL);
+
+				
+				tblPrices.addCol("mtl_avg",COL_TYPE_ENUM.COL_DOUBLE);
+				
+				tblPrices.addCol("min_mtl_px",COL_TYPE_ENUM.COL_DOUBLE);
+				tblPrices.addCol("max_mtl_px",COL_TYPE_ENUM.COL_DOUBLE);
+				
+				tblPrices.setColFormatAsDouble("mtl_avg", 2, 2);
+				tblPrices.setColFormatAsDouble("min_mtl_px", 2, 2);
+				tblPrices.setColFormatAsDouble("min_mtl_px", 2, 2);
+				
+				Table tblMetal = Table.tableNew();
+				tblMetal.select(tblPrices,"DISTINCT,metal","ref_source_id GT 0");
+				
+				for(int i = 1;i<=tblMetal.getNumRows();i++){
+					
+					Table tblCurrMtl = Table.tableNew();
+					
+					tblCurrMtl.select(tblPrices, "*", "metal EQ " + tblMetal.getString("metal",i) );
+
+					double dblSum = tblCurrMtl.sumRangeDouble("price", 1, tblCurrMtl.getNumRows());
+					double dblNumDays = Double.valueOf(tblCurrMtl.getNumRows()); 
+					double dblAvg = dblSum /dblNumDays;
+					
+					tblCurrMtl.setColValDouble("mtl_avg",dblAvg);
+					
+					tblPrices.select(tblCurrMtl, "mtl_avg", "metal EQ $metal AND reset_date EQ $reset_date AND ref_source_id EQ $ref_source_id " );
+
+					
+					double dblMinPrice = tblCurrMtl.getDouble("price",1);
+					for(int j = 1;j<=tblCurrMtl.getNumRows();j++){
+						
+						if(tblCurrMtl.getDouble("price", j)<dblMinPrice){
+							dblMinPrice = tblCurrMtl.getDouble("price",j);
+						}
+					}
+					
+					tblCurrMtl.setColValDouble("min_mtl_px",dblMinPrice);
+					
+					tblPrices.select(tblCurrMtl, "min_mtl_px", "metal EQ $metal AND reset_date EQ $reset_date AND ref_source_id EQ $ref_source_id " );
+					
+					
+					double dblMaxPrice = tblCurrMtl.getDouble("price",1);
+					for(int j = 1;j<=tblCurrMtl.getNumRows();j++){
+						
+						if(tblCurrMtl.getDouble("price", j)>dblMaxPrice){
+							dblMaxPrice = tblCurrMtl.getDouble("price",j);
+						}
+					}
+
+					tblCurrMtl.setColValDouble("max_mtl_px",dblMaxPrice);
+					tblPrices.select(tblCurrMtl, "max_mtl_px", "metal EQ $metal AND reset_date EQ $reset_date AND ref_source_id EQ $ref_source_id " );
+					
+					
+					tblCurrMtl.destroy();
+				}
+
+				
+				tblPrices.delCol("reset_date");
+				tblPrices.delCol("price");
+				tblPrices.delCol("ref_source_id");
+
+
+				tblPrices.makeTableUnique();
+				
+				returnt.select(tblPrices, "PREORDERED,*", "metal NE ''");
 				
 				
-				tblCurrMtl.destroy();
+				tblPrices.destroy();
+
+				
 			}
-
 			
-			tblPrices.delCol("reset_date");
-			tblPrices.delCol("price");
-			tblPrices.delCol("ref_source_id");
-
-
-			tblPrices.makeTableUnique();
-			
-			returnt.select(tblPrices, "PREORDERED,*", "metal NE ''");
-			
-			
-			tblPrices.destroy();
 			
 		} catch (Throwable ex) {
 			OConsole.oprint(ex.toString());
