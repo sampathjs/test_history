597a2aa1aef4e2aaac9d3b6a2205fa7b407783cd
Arjit.Agrawal <arjit.agrawal@matthey.com>
2020-01-17 19:40:45 +0530

    Deleting entry for UseCache from user_const_repo

--- StandingDataSQL/USER_const_repository-epi-1037-p1685-auto-assign-SI.sql
+++ StandingDataSQL/USER_const_repository-epi-1037-p1685-auto-assign-SI.sql
@@ -1,17 +1,18 @@
 begin tran
 
 select count(*) from USER_const_repository
-
 go
+
+DELETE FROM dbo.USER_const_repository WHERE context = 'FrontOffice' AND sub_context = 'Auto SI Population' AND name = 'useCache'
+GO
 
 UPDATE dbo.USER_const_repository SET string_value = 'INFO' WHERE context = 'FrontOffice' AND sub_context = 'Auto SI Population' AND name = 'logLevel'
+GO
 
-go
 select count(*) from USER_const_repository
+GO
 
-go
 commit tran
-
 go
 
 select * from USER_const_repository where [sub_context] like '%Auto SI Population%'
