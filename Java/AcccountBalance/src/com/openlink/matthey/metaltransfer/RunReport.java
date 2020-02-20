6eadb4cea2d511b3c015c6980f1ce538840aecbb
Arjit.Agrawal <arjit.agrawal@matthey.com>
2020-02-20 11:48:51 +0530

    Modified script to update report param name & data sources for improving performance

--- Java/AcccountBalance/src/com/openlink/matthey/metaltransfer/RunReport.java
+++ Java/AcccountBalance/src/com/openlink/matthey/metaltransfer/RunReport.java
@@ -15,15 +15,11 @@ import com.olf.embedded.application.EnumScriptCategory;
 import com.olf.embedded.application.ScriptCategory;
 import com.olf.embedded.trading.AbstractTradeInputScript;
 import com.olf.jm.logging.Logging;
-import com.olf.openjvs.OConsole;
 import com.olf.openrisk.table.Table;
 import com.olf.openrisk.trading.EnumTransactionFieldId;
 import com.olf.openrisk.trading.Field;
 import com.olf.openrisk.trading.Transaction;
 import com.olf.openrisk.trading.Transactions;
-import com.openlink.endur.utilities.logger.LogCategory;
-import com.openlink.endur.utilities.logger.LogLevel;
-import com.openlink.endur.utilities.logger.Logger;
 
 /*
  * History:
@@ -40,6 +36,7 @@ import com.openlink.endur.utilities.logger.Logger;
  */
 @ScriptCategory({ EnumScriptCategory.TradeInput })
 public class RunReport extends AbstractTradeInputScript {
+	
 	@Override
 	public Table execute(Context context, Transactions transactions) {
 		
@@ -57,9 +54,10 @@ public class RunReport extends AbstractTradeInputScript {
 			negativeThreshold.setValue("");
 			fromAccountBalance.setValue("");
 			fromAccountFinalBalance.setValue("");
+			
 			{
 				Table thresholdLookup = DataAccess.getDataFromTable(context, "SELECT * from USER_JM_negative_threshold");
-				if (null!=thresholdLookup && thresholdLookup.getRowCount() ==1) {
+				if (null != thresholdLookup && thresholdLookup.getRowCount() == 1) {
 					negativeThreshold.setValue(thresholdLookup.getDouble("value", 0));
 				}
 			}
@@ -69,42 +67,52 @@ public class RunReport extends AbstractTradeInputScript {
 					|| null == unit  || metalCode.isEmpty()
 					|| null == settleDate ) {
 				Logging.info("NOT enough information to check balance!");
-
 			} else {
-
-				String  accountId ="";
-				Table accountLookup = DataAccess.getDataFromTable(context, String.format("SELECT account_number FROM account where account_name='%s'",fromAccount.trim()));
-				if (null!=accountLookup && accountLookup.getRowCount() ==1) {
-					accountId = accountLookup.getString("account_number", 0);
-				}
-				Logging.info(
-						String.format("From %s>%s<, %s, in %s  on %s",
-								accountId,fromAccount,
-								metalCode,
-								unit,
-								settleDate.getDisplayString()));
-				Date stlDate = settleDate.getValueAsDate();
-				SimpleDateFormat sdf = new SimpleDateFormat ("dd-MMM-yyyy");
-				Map<String, String> params = new HashMap<String, String>();
-				params.put("report_name", "Account Balance Retrieval");
-				params.put("acct_num", accountId);
-				params.put("ReportDate", sdf.format(stlDate));
-				params.put("ReportingUnit", unit);
-				params.put("metalCode", metalCode);
+				String accountId = "";
+				Table accountLookup = null;
+				Table answer = null;
 				
-				double settleDateBalance = 0.0d;
-				Table answer = processRBprocessCustomRequest(context, null, params);
-				//context.getDebug().viewTable(answer);
-				if (null != answer && answer.getRowCount() == 1)
-					settleDateBalance = answer.getDouble("balance", 0);
-				Logging.info(
-						String.format("Xfer From %s balance(%f) on SettleDate %s in %s",
-								accountId, 
-								settleDateBalance,
-								settleDate.getValueAsString(),
-								unit));
-				fromAccountBalance.setValue(settleDateBalance);
-				fromAccountFinalBalance.setValue(settleDateBalance-transferQuantity.getValueAsDouble());
+				try {
+					accountLookup = DataAccess.getDataFromTable(context, 
+							String.format("SELECT account_id FROM account WHERE account_name='%s'", fromAccount.trim()));
+					
+					if (null != accountLookup && accountLookup.getRowCount() == 1) {
+						accountId = String.valueOf(accountLookup.getInt("account_id", 0));
+					}
+					
+					Logging.info(String.format("From %s>%s<, %s, in %s  on %s",
+									accountId,fromAccount, metalCode, unit, settleDate.getDisplayString()));
+					
+					Date stlDate = settleDate.getValueAsDate();
+					SimpleDateFormat sdf = new SimpleDateFormat ("dd-MMM-yyyy");
+					
+					Map<String, String> params = new HashMap<String, String>();
+					params.put("report_name", "Account Balance Retrieval");
+					params.put("ReportAccount", accountId);
+					params.put("ReportDate", sdf.format(stlDate));
+					params.put("ReportingUnit", unit);
+					params.put("metalCode", metalCode);
+					
+					double settleDateBalance = 0.0d;
+					answer = processRBprocessCustomRequest(context, null, params);
+
+					if (null != answer && answer.getRowCount() == 1)
+						settleDateBalance = answer.getDouble("balance", 0);
+					
+					Logging.info(String.format("Xfer From %s balance(%f) on SettleDate %s in %s",
+									accountId, settleDateBalance, settleDate.getValueAsString(), unit));
+					
+					fromAccountBalance.setValue(settleDateBalance);
+					fromAccountFinalBalance.setValue(settleDateBalance-transferQuantity.getValueAsDouble());
+					
+				} finally {
+					if (accountLookup != null) {
+						accountLookup.dispose();
+					}
+					if (answer != null) {
+						answer.dispose();
+					}
+				}
 			}
 		}
 		
@@ -117,15 +125,12 @@ public class RunReport extends AbstractTradeInputScript {
 	 * wrapper to submit request to ReportBuilder and return the results of that
 	 * processing
 	 */
-	private Table processRBprocessCustomRequest(Context context, Table argumentsTable,
-			Map<String, String> parameters) {
-
+	private Table processRBprocessCustomRequest(Context context, Table argumentsTable, Map<String, String> parameters) {
 		Table output = null;
 		Table resultData = null;
 		int result = 0;
+		
 		try {
-
-			// System.out.println("Params..." + parameters.toString());
 			IReportParameters newParameters = new ReportParameters(context, parameters);
 
 			GenerateAndOverrideParameters balances = new GenerateAndOverrideParameters(context, newParameters);
@@ -136,8 +141,9 @@ public class RunReport extends AbstractTradeInputScript {
 			}
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			Logging.error("Error in executing report", e);
 		}
+		
 		if (result < 1) {
 			Logging.info(String.format("%s Didn't return any results!!!", parameters.get(IRequiredParameters.NAME_PARAMETER)));
 			return output;
@@ -150,8 +156,7 @@ public class RunReport extends AbstractTradeInputScript {
 				Logging.info(String.format("RESULT:" + resultData.asXmlString()));
 				
 			} catch (Exception e) {
-				Logging.info(String.format("\t Report Dispatcher error cloning result!"), e);
-				e.printStackTrace();
+				Logging.error("\t Report Dispatcher error cloning result!", e);
 			}
 			return output;
 		}
