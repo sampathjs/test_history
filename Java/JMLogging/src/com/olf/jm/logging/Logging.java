cd80499dfa5cf0de59785c196bf7eb6e4b7c917d
JOHNSONMATTHEY\SharmD02 <deepika.sharma@matthey.com>
2020-02-20 13:57:21 +0000

    updated JM Logging with common method

--- Java/JMLogging/src/com/olf/jm/logging/Logging.java
+++ Java/JMLogging/src/com/olf/jm/logging/Logging.java
@@ -57,88 +57,51 @@ public class Logging {
      * @param Constants repository sub-context
      */
     public static void init(Session session, Class<?> plugin, String context, String subcontext) {
-        // Levels handle multiple initialisations via nested plugins
-        // Each level gets it's own logger which logs to it's own log file
-        // Each initialisation increments the level and a close decrements the level
-        level++;
-        Logging logger = new Logging();
-        logMap.put(level, logger);
-
-        String logLevel = "Error";
-        String logFile = plugin.getSimpleName() + ".log";
-        String logDir = null;
- 
-		try {
-    		ConstRepository constRep = new ConstRepository(context, subcontext);
-    
-    		logLevel = constRep.getStringValue("logLevel", logLevel);
-    		logFile = constRep.getStringValue("logFile", logFile);
-    		logDir = constRep.getStringValue("logDir", session.getIOFactory().getReportDirectory());
-    	} catch (Exception e) {
-    		throw new RuntimeException("Error initialising logging. ", e);
-		}
- 
-		logger.serverName = String.format("%1$-16.16s", session.getHostName());
+      try {
+        Logging logger = logInitialiser(plugin, context, subcontext);
+        logger.serverName = String.format("%1$-16.16s", session.getHostName());
         logger.userName = String.format("%1$-10.10s", session.getUser().getName());
         logger.sessionId = String.format("S%1$-5.5s", session.getSessionId());
         logger.processId = String.format("P%1$-5.5s", session.getProcessId());
-        logger.logfile = logDir + '/' + logFile;
-        
         Logging.info("Process started.");
+      } catch (Exception e) {
+        throw new RuntimeException("Error initialising logging. ", e);
+      }
+
     }
 
     /**
-	 * Initialise logging to support both OpenJVS & OpenComponent
-	 * 
-	 *
-	 * @param plugin
-	 *            Class of main entry plugin
-	 * @param Constants
-	 *            repository context
-	 * @param Constants
-	 *            repository sub-context
-	 */
-	public static void init(Class<?> plugin, String context, String subcontext) {
-		// Levels handle multiple initialisations via nested plugins
-		// Each level gets it's own logger which logs to it's own log file
-		// Each initialisation increments the level and a close decrements the
-		// level
-		level++;
-		Logging logger = new Logging();
-		logMap.put(level, logger);
+     * Initialise logging to support both OpenJVS & OpenComponent
+     * 
+     *
+     * @param plugin
+     *            Class of main entry plugin
+     * @param Constants
+     *            repository context
+     * @param Constants
+     *            repository sub-context
+     */
+    public static void init(Class<?> plugin, String context, String subcontext) {
+      try {
+        Logging logger = logInitialiser(plugin, context, subcontext);
+        logger.serverName = String.format("%1$-16.16s", Ref.getInfo().getString("hostname", 1));
+        logger.userName = String.format("%1$-10.10s", Ref.getUserName());
+        logger.sessionId = String.format("S%1$-5.5s", Ref.getInfo().getInt("session_sequence", 1));
+        logger.processId = String.format("P%1$-5.5s", Ref.getProcessId());
+        Logging.info("Process started.");
+      } catch (Exception e) {
+        throw new RuntimeException("Error initialising logging. ", e);
+      }
+    }
 
-		String logLevel = "Error";
-		String logFile = plugin.getSimpleName() + ".log";
-		String logDir = null;
-
-		try {
-			ConstRepository constRep = new ConstRepository(context, subcontext);
-
-			logLevel = constRep.getStringValue("logLevel", logLevel);
-			logFile = constRep.getStringValue("logFile", logFile);
-			logDir = constRep.getStringValue("logDir", logDir);
-
-			logger.serverName = String.format("%1$-16.16s", Ref.getInfo().getString("hostname", 1));
-			logger.userName = String.format("%1$-10.10s", Ref.getUserName());
-			logger.sessionId = String.format("S%1$-5.5s", Ref.getInfo().getInt("session_sequence", 1));
-			logger.processId = String.format("P%1$-5.5s", Ref.getProcessId());
-			logger.logfile = logDir + '/' + logFile;
-
-		} catch (Exception e) {
-			throw new RuntimeException("Error initialising logging. ", e);
-		}
-
-		Logging.info("Process started.");
-	}
-
-	/**
-	 * Log info message.
-	 * 
-	 * @param message
-	 *            Log message
-	 * @param args
-	 *            Arguments for formatted message
-	 */
+    /**
+     * Log info message.
+     * 
+     * @param message
+     *            Log message
+     * @param args
+     *            Arguments for formatted message
+     */
     public static void info(String message, Object...args) {
         logMap.get(level).logMessage("INFO", message, null, args);
     }
@@ -164,7 +127,7 @@ public class Logging {
      */
     private void logMessage(String level, String message, Throwable exc, Object...args) {
 
-    	String stackTrace = null;
+        String stackTrace = null;
         if (exc != null) {
             try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
                  PrintStream ps = new PrintStream(baos)) {
@@ -176,11 +139,11 @@ public class Logging {
                }
         }
 
-    	try {
-    	    String logMessage = message;
-    	    if (args.length > 0) {
-    	        logMessage = String.format(message, args);
-    	    }
+        try {
+            String logMessage = message;
+            if (args.length > 0) {
+                logMessage = String.format(message, args);
+            }
             Files.write(Paths.get(logfile), (getPrefix(level) + logMessage + '\n').getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
             if (stackTrace != null) {
                 Files.write(Paths.get(logfile), stackTrace.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
@@ -249,5 +212,30 @@ public class Logging {
         Logging.info("Process finished");
         logMap.remove(level);
         level--;
+    }
+    private static Logging logInitialiser(Class<?> plugin, String context, String subcontext) {
+      // Levels handle multiple initialisations via nested plugins
+      // Each level gets it's own logger which logs to it's own log file
+      // Each initialisation increments the level and a close decrements the level
+      level++;
+      Logging logger = new Logging();
+      logMap.put(level, logger);
+
+      String logLevel = "Error";
+      String logFile = plugin.getSimpleName() + ".log";
+      String logDir = null;
+
+      try {
+        ConstRepository constRep = new ConstRepository(context, subcontext);
+
+        logLevel = constRep.getStringValue("logLevel", logLevel);
+        logFile = constRep.getStringValue("logFile", logFile);
+        logDir = constRep.getStringValue("logDir", logDir);
+        logger.logfile = logDir + '/' + logFile;
+        return logger;
+      } catch (Exception e) {
+        throw new RuntimeException("Error initialising logging. ", e);
+      }
+
     }
 }
