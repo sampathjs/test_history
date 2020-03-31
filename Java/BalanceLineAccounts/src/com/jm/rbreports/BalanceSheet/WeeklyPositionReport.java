5258d6403823c67de58e695ad79596050fa48ec4
WaliaJ01 <WaliaJ01@GBROMEOLC02D.johnsonmatthey.com>
2020-03-31 12:53:02 +0100

    SR 315235 - Output of 3 fromt office reports emailed through this change

---
+++ Java/BalanceLineAccounts/src/com/jm/rbreports/BalanceSheet/WeeklyPositionReport.java
@@ -0,0 +1,291 @@
+/********************************************************************************
+
+ * Script Name: WeeklyPositionReport
+ * Script Type: Main
+ * 
+ * The script will run 3 front office reports and sends the combined output in an email to the user list
+ * 
+ * Revision History:
+ * Version Date       Author      Description
+ * 1.0     14-Apr-20  Jyotsna	  Initial Version - Developed as part of SR 315235
+ ********************************************************************************/
+package com.jm.rbreports.BalanceSheet;
+
+import java.util.HashSet;
+
+import com.olf.openjvs.DBUserTable;
+import com.olf.openjvs.IContainerContext;
+import com.olf.openjvs.IScript;
+import com.olf.openjvs.OCalendar;
+import com.olf.openjvs.OException;
+import com.olf.openjvs.Ref;
+import com.olf.openjvs.ReportBuilder;
+import com.olf.openjvs.Services;
+import com.olf.openjvs.Table;
+import com.olf.openjvs.Util;
+import com.olf.openjvs.enums.DATE_FORMAT;
+import com.olf.openjvs.enums.OLF_RETURN_CODE;
+import com.olf.openjvs.enums.SHM_USR_TABLES_ENUM;
+import com.olf.openjvs.enums.TABLE_SORT_DIR_ENUM;
+import com.openlink.util.constrepository.ConstRepository;
+import com.openlink.util.logging.PluginLog;
+
+public class WeeklyPositionReport implements IScript {
+	private static final String CONTEXT = "Reports";
+	private ConstRepository repository = null;
+	private String toList;
+	private StringBuilder mailSignature;
+	private String taskName;
+	private String emailContent;
+	private String mailServiceName;
+	private static final String PRM_NAME_RPT_DATE = "ReportDate";
+	private Table reportList;
+	private int rptDate;
+	boolean showZeros = false;
+	private StringBuilder reportOutputString = new StringBuilder();
+	private String alertEmailID;
+
+
+	/**
+	 * to initialise variables
+	 * 
+	 * @param none
+	 * 
+	 * @return none
+	 */
+	private void init() throws OException {
+		Table task = Ref.getInfo();
+		try {
+
+			taskName = task.getString("task_name", 1);
+			repository = new ConstRepository(CONTEXT, taskName);
+			com.matthey.utilities.Utils.initPluginLog(repository, taskName);
+			mailServiceName = repository.getStringValue("mailServiceName");
+
+			//Checking if Mail service is running under Domain services
+			int online = Services.isServiceRunningByName(mailServiceName);
+			if (online == 0){
+				PluginLog.error("Exception occured while running the task:  " + taskName + " Mail service offline");
+				throw new OException("Mail service offline");
+			}else{
+				PluginLog.info(" Initializing variables..");
+				String listOfUsers;
+				listOfUsers = repository.getStringValue("listOfUsers");
+				toList = com.matthey.utilities.Utils.convertUserNamesToEmailList(listOfUsers);
+				emailContent = repository.getStringValue("emailContent");
+				reportList = repository.getMultiStringValue("Report Name");
+				reportList.sortCol("value",TABLE_SORT_DIR_ENUM.TABLE_SORT_DIR_ASCENDING);
+				rptDate =  OCalendar.today();
+				reportOutputString.append(emailContent);
+				alertEmailID = repository.getStringValue("endurSupportID");
+
+			}
+		} 
+		catch (Exception e) {
+			PluginLog.error("Exception occured while initialising variables " + e.getMessage());
+			throw new OException(e);
+		}
+		finally {
+			task.destroy();
+		}
+	}
+	private void sendAlert() throws OException{
+		PluginLog.info("errors while sending email to: " + toList + ", please check log file for more details\n");
+		String emailBody = repository.getStringValue("alertEmailBody") + taskName +  " e-mail not sent to the following reciepients: <BR>" + toList + "<BR> Please check error logs" + mailSignature;
+		String logDir   = repository.getStringValue("logDir");
+		String fileToAttach = logDir +"\\" + taskName +".log"; 
+		com.matthey.utilities.Utils.sendEmail(alertEmailID, taskName + " Failed : Requires SUPPORT Attention! ",emailBody, fileToAttach, mailServiceName);
+		Util.scriptPostStatus(" Mail not sent"); 
+		Util.exitFail();
+		
+	}
+	public void execute(IContainerContext context) throws OException{
+
+		init(); //initialising variables
+		PluginLog.info("Total no of reports to be run: " + reportList.getNumRows());
+		try{
+		for (int reportCount = 1; reportCount<=reportList.getNumRows();reportCount++){
+
+			String reportName = reportList.getString("value", reportCount);
+			PluginLog.info("Started running Report \"" + reportCount + '"'+ " : " + reportName);
+
+
+			Table reportOutput = new Table(); 
+			
+			if(reportName.equalsIgnoreCase("Futures Breakdown")){
+
+				ReportBuilder rptBuilder = ReportBuilder.createNew(reportName);
+				rptBuilder.setOutputTable(reportOutput);
+				rptBuilder.runReport();
+				rptBuilder.dispose();
+				reportOutput.setColFormatAsRef("internal_portfolio", SHM_USR_TABLES_ENUM.PORTFOLIO_TABLE);				
+				reportOutput.convertColToString(1);
+				reportOutput.setColTitle(1, "Portfolio");
+
+			}
+			else {
+				reportOutput = runReport(reportName, rptDate);	
+			}
+
+			PluginLog.info("Generated report " + reportName);
+
+			String htmlBody;
+			if(reportName.equals("Stock Split by Form")){
+				//using specific function to convert JVS table to HTML string for Stock report
+				htmlBody = convertStockReporttoHTMLString(reportOutput,showZeros,reportName);
+
+			} else {
+				// using generic function to convert JVS table to HTML string
+				htmlBody = com.matthey.utilities.Utils.convertTabletoHTMLString(reportOutput,showZeros,reportName);
+			}
+			PluginLog.info( reportName + " output converted to html string successfully\n");
+			reportOutputString.append(htmlBody);
+		}
+ 
+		mailSignature = com.matthey.utilities.Utils.standardSignature(); 
+		reportOutputString.append(mailSignature);
+		PluginLog.info("Sending out email to : " + toList);
+		boolean retVal = com.matthey.utilities.Utils.sendEmail(toList, taskName, reportOutputString.toString(), "", mailServiceName);
+		if(retVal){
+			PluginLog.info("Mail Sent Successfully\n");
+		}
+		else{
+			//send alert to support group
+			sendAlert();
+		}
+		}
+		
+		catch (Exception e) {
+			PluginLog.error("Exception occured while running task " + taskName + e.getMessage());
+			throw new OException(e);
+		}
+	}
+	/**
+	 * method: convertStockReporttoHTMLString
+	 * function to convert output of Stock Split  by Form report output to HTML string
+	 * @param Table, boolean, string
+	 * 
+	 * @return StringBuilder
+	 */
+	private String convertStockReporttoHTMLString(Table tbl,boolean showZeros, String reportName) throws OException{
+		StringBuilder emailBody = new StringBuilder("<br><br><h3>" + reportName + "</h3>");
+
+		emailBody.append("<table border = 1>");
+
+		//set up table header
+		PluginLog.info("Setting up Table Header ");
+		emailBody.append("<tr><b>");
+		for(int cols = 1; cols<=tbl.getNumCols();cols++){
+			String colHeader = tbl.getColTitle(cols);
+			if(cols==1){//read colorcode from const repo
+				emailBody.append("<th style = 'width: 20%' bgcolor = '#add1cf'>" + colHeader + "</th>");
+			}else{
+				emailBody.append("<th style = 'width: 10%' bgcolor = '#add1cf'>" + colHeader + "</th>");	
+			}
+				            	
+		}
+		emailBody.append("</b></tr>");
+		//set up table contents
+		int rows = tbl.getNumRows();
+		ConstRepository cr =  new ConstRepository(CONTEXT,taskName);
+
+		Table formType = Util.NULL_TABLE;
+		formType = cr.getMultiStringValue("Form Type");
+		HashSet<String> formSet = new HashSet<>();
+		for (int rowcount=1;rowcount<=formType.getNumRows();rowcount++){
+
+			String cflowtype = formType.getString("value", rowcount);
+			formSet.add(cflowtype);            
+
+		}
+		
+		PluginLog.info("Setting up Table rows ");
+		for(int row = 1; row <= rows; row++) {
+			String form = tbl.getString("balance_desc", row);
+			if(formSet.contains(form.trim())){
+				PluginLog.info("Setting up non-aggregated row ");
+				emailBody.append("<tr>");      		 
+			}else{
+				PluginLog.info("Setting up aggregated row ");
+				emailBody.append("<tr bgcolor = '#d0e6e4'>");
+			}
+
+			for(int cols = 1; cols<=tbl.getNumCols();cols++){
+				int colType = tbl.getColType(cols);
+				String colName = tbl.getColName(cols);
+				
+				
+				 try{
+				switch(colType){
+				case 0 :
+					int intRowValue = tbl.getInt(colName, row);
+					if(showZeros == false && intRowValue==0 ){
+						emailBody.append("<td>").append("").append("</td>");
+					}else{
+						emailBody.append("<td align = 'center'>").append(intRowValue).append("</td>");
+					}
+					break;
+				case 2:
+					String strRowValue = tbl.getString(colName, row);
+					if(formSet.contains(strRowValue.trim()) ){
+						strRowValue = "     " + strRowValue;
+						emailBody.append("<td align = 'center'>").append(strRowValue).append("</td>");
+					}else if(strRowValue.startsWith("L1")){
+						emailBody.append("<td>").append(strRowValue).append("</td>");
+					}else {
+						emailBody.append("<td align = 'right'>").append(strRowValue).append("</td>");
+					}
+					break;
+				}
+				 }
+				 catch (Exception e) {
+						PluginLog.error("Exception occured while  " + taskName + e.getMessage());
+						throw new OException(e);
+					}
+				 
+			}
+			emailBody.append("</tr>");
+		}
+
+		emailBody.append("</table><br>");
+		return emailBody.toString();
+	}
+
+	/**
+	 * Method runReport
+	 * Create report showing aggregated balances by balance line/metal group
+	 * @param rptName: Report Name rptDate: reporting date 
+	 * @throws OException 
+	 */
+	protected Table runReport(String rptName, int rptDate) throws OException
+	{
+		PluginLog.info("Generating report \"" + rptName + '"');
+		ReportBuilder rptBuilder = ReportBuilder.createNew(rptName);
+
+		int retval = rptBuilder.setParameter("ALL", PRM_NAME_RPT_DATE, OCalendar.formatJd(rptDate, DATE_FORMAT.DATE_FORMAT_DMLY_NOSLASH));
+		if (retval != OLF_RETURN_CODE.OLF_RETURN_SUCCEED.toInt()) {
+			String msg = DBUserTable.dbRetrieveErrorInfo(retval,
+					"Failed to set parameter report date for report \"" + rptName + '"');
+			throw new RuntimeException(msg);
+		}
+
+		Table output = new Table();  
+		rptBuilder.setOutputTable(output);
+
+		retval = rptBuilder.runReport();
+		if (retval != OLF_RETURN_CODE.OLF_RETURN_SUCCEED.toInt()) {
+			String msg = DBUserTable.dbRetrieveErrorInfo(retval, "Failed to generate report \"" + rptName + '"');
+			throw new RuntimeException(msg);
+		}
+		PluginLog.info("Generated report " + rptName);
+		rptBuilder.dispose();
+
+		return output;
+	}
+
+	
+	//utility funtion -  add description and exception hadnling
+	
+	//TODO throw exception is table is invalid
+	
+}
--- Java/Utilities/src/com/matthey/utilities/Utils.java
+++ Java/Utilities/src/com/matthey/utilities/Utils.java
@@ -8,6 +8,7 @@
  * 1.0     			  	Arjit Aggarwal	  	Initial Version
  * 1.1		18-Sept-19  Jyotsna Walia		Added utility function for sending email
  * 1.2      23-Jan-2020 Pramod Garg	        Added utility function to send email for multiple attachments
+ * 1.3		14-Apr-20	Jyotsna Walia		Added  utility function to convert a jvs table to HTML string, supports and double type columns 
  ********************************************************************************/
 
 package com.matthey.utilities;
@@ -19,9 +20,13 @@ import java.util.List;
 import com.matthey.utilities.enums.Region;
 import com.olf.openjvs.DBaseTable;
 import com.olf.openjvs.EmailMessage;
+import com.olf.openjvs.OCalendar;
 import com.olf.openjvs.OException;
+import com.olf.openjvs.Ref;
 import com.olf.openjvs.Table;
+import com.olf.openjvs.Util;
 import com.olf.openjvs.enums.EMAIL_MESSAGE_TYPE;
+import com.openlink.util.constrepository.ConstRepository;
 import com.openlink.util.logging.PluginLog;
 public class Utils {
 	
@@ -205,5 +210,125 @@ public class Utils {
 		
 	}
 	
-	
+	static public void initPluginLog(ConstRepository cr, String dfltFname) throws OException
+	{
+		String logLevel = "Error"; 
+		String logFile  = dfltFname + ".log"; 
+		String logDir   = null;
+		String useCache = "No";
+ 
+        try
+        {
+        	logLevel = cr.getStringValue("logLevel", logLevel);
+        	logFile  = cr.getStringValue("logFile", logFile);
+        	logDir   = cr.getStringValue("logDir", logDir);
+        	useCache = cr.getStringValue("useCache", useCache);            
+
+            if (logDir == null)
+            {
+            	PluginLog.init(logLevel);
+            }
+            else
+            {
+            	PluginLog.init(logLevel, logDir, logFile);
+            }
+        }
+        catch (Exception e)
+        {
+        	String msg = "Failed to initialise log file: " + logDir + "\\" + logFile;
+        	throw new OException(msg);
+        }
+	}
+	static public StringBuilder standardSignature()throws OException{
+		Table envInfo = Util.NULL_TABLE;
+		envInfo = com.olf.openjvs.Ref.getInfo();
+		 Table refInfo = Ref.getInfo();
+	      
+	           
+	           String taskName      = refInfo.getString("task_name", 1);
+		
+		StringBuilder signature = new StringBuilder();
+
+		if (Table.isTableValid(envInfo) != 1) {
+			throw new OException("Invalid table:  envInfo");
+		}
+		try{
+		signature.append("<p style='color:gray;'>This information has been generated from </BR>Database: " + envInfo.getString("database", 1) + "</p>");
+		signature.append("<p style='color:gray;'>On Server: " + envInfo.getString("server", 1) + "</p>");
+		signature.append("<p style='color:gray;'>From Task: " + taskName + "</p>");
+		signature.append("<i>Endur Trading date: "+ OCalendar.formatDateInt(Util.getTradingDate()) + "</i>");
+		signature.append("</BR><i>Endur Business date: " + OCalendar.formatDateInt(Util.getBusinessDate()) + "</i></BR>");
+		}
+		catch (Exception e) {
+			PluginLog.error("Exception occured while initialising variables " + e.getMessage());
+			throw new OException(e);
+		}
+		finally {
+			envInfo.destroy();
+		}
+		
+		return signature;
+	}
+	//Utility function to convert a JVS table into html string
+	static	public String convertTabletoHTMLString(Table tbl,boolean showZeros, String reportName) throws OException{
+
+			StringBuilder emailBody = new StringBuilder("<br><br><h3>" + reportName + "</h3>");
+
+			emailBody.append("<table border = 1>");
+			PluginLog.info("Total no. of columns: " + tbl.getNumCols());
+			//set up table header
+			emailBody.append("<tr><b>");
+			try{
+			for(int cols = 1; cols<=tbl.getNumCols();cols++){
+				String colHeader = tbl.getColTitle(cols);
+				emailBody.append("<th bgcolor = '#add1cf'>" + colHeader + "</th>");
+				PluginLog.info("Added column header  " + colHeader);	            	
+			}	            
+			emailBody.append("</b></tr>");
+			//set up table contents
+			int rows = tbl.getNumRows();
+			PluginLog.info("Total no. of rows: " + rows);
+			for(int row = 1; row <= rows; row++) {
+
+
+				emailBody.append("<tr>");
+				for(int cols = 1; cols<=tbl.getNumCols();cols++){
+					int colType = tbl.getColType(cols);
+					String colName = tbl.getColName(cols);
+					
+					switch(colType){
+					case 0:
+						int intRowValue = tbl.getInt(colName, row);
+						if(showZeros == false && intRowValue==0 ){
+							emailBody.append("<td>").append("").append("</td>");
+						}else{
+							emailBody.append("<td align = 'center'>").append(intRowValue).append("</td>");
+						}
+						break;
+					case 2:
+						String strRowValue = tbl.getString(colName, row);
+						if(strRowValue == null || strRowValue.trim().isEmpty() || strRowValue.equalsIgnoreCase("null") ){
+							strRowValue = "";
+
+						}
+						emailBody.append("<td align = 'center'>").append(strRowValue).append("</td>");
+						break;
+					}
+				}
+				emailBody.append("</tr>");
+			}
+
+			emailBody.append("</table><br>");
+			}
+			catch (Exception e) {
+				PluginLog.error("Exception occured while converting JVS table to HTML string\n " + e.getMessage());
+				throw new OException(e);
+			}
+			finally {
+				tbl.destroy();
+			}
+			return emailBody.toString();
+		}  
+
+    	
 }
