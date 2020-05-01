d438eb513e752db6f77bd4dea7e47dd22d33360a
Jens Waetcher <Jens.Waetcher@matthey.com>
2020-05-01 09:03:29 +0100

    LimitsReporting Kotlin to Java. Added missing save for intraday results
    # Conflicts:
    #	Java/LimitsReporting/src/main/java/com/matthey/pmm/limits/reporting/translated/LimitsReportingIntradayChecker.java

--- Java/LimitsReporting/src/main/java/com/matthey/pmm/limits/reporting/translated/LimitsReportingIntradayChecker.java
+++ Java/LimitsReporting/src/main/java/com/matthey/pmm/limits/reporting/translated/LimitsReportingIntradayChecker.java
@@ -17,7 +17,55 @@ public class LimitsReportingIntradayChecker {
     public List<RunResult> run() {
         PluginLog.info("checking liquidity limits");
 
-        List<RunResult> intradayResults = dealingLimitChecker.check(DealingLimitType.INTRADAY_DESK);
+        List<RunResult> intradayResults = new ArrayList<>();
+        for (LiquidityLimit liquidityLimit : connector.getLiquidityLimits()) {
+            PluginLog.info("checking liquidity limit: " + liquidityLimit);
+            double fxRate = connector.getGbpUsdRate(connector.getRunDate());
+            PluginLog.info("GBP/USD rate: " + fxRate);
+            Map<String, Double> metalPrices = connector.getMetalPrices();
+            PluginLog.info("metal prices: " + metalPrices);
+            MetalBalances metalBalances = connector.getMetalBalances();
+            double liquidity = metalBalances.getBalance(runType, liquidityLimit.getMetal());
+            boolean breach = !(   liquidity >= liquidityLimit.getLowerLimit() 
+            		           && liquidity <= liquidityLimit.getUpperLimit());
+            boolean breachLowerLimit = liquidity < liquidityLimit.getLowerLimit();
+            int diff = (int)(!breach?0:(breachLowerLimit?
+            		liquidityLimit.getLowerLimit() - liquidity:
+            		liquidity - liquidityLimit.getUpperLimit()
+            		));
+            double breachTOz = getBreachBalancesInTOz(metalBalances, liquidityLimit.getMetal(), diff);
+            PluginLog.info("breach in TOz: " + breachTOz);
+            double liability = breachTOz * metalPrices.get(liquidityLimit.getMetal()) / fxRate;
+            PluginLog.info("liability: " + liability);            
+            boolean critical = !breachLowerLimit && liability > liquidityLimit.getMaxLiability();
+            String liquidityBreachLimit = breach?(breachLowerLimit?"Lower Limit":"UpperLimit"):"";
+            
+            RunResult runResult =   new RunResult (
+            		connector.getRunDate(), // runTime
+            		runType, // runType
+            		"", // desk
+            		liquidityLimit.getMetal(), // metal
+            		liquidityLimit.getLowerLimit(), // liquidityLowerLimit
+            		liquidityLimit.getUpperLimit(), // liquidityUpperLimit
+            		liquidityLimit.getMaxLiability(), // liquidityMaxLiability
+            		0, // position limit
+            		breach, // breach
+            		liquidityBreachLimit, // liquidityBreachLimit
+            		liquidity, // currentPosition
+            		diff, // liquidityDiff
+            		breachTOz, // breachTOz
+            		liability, // breachGBP
+            		critical,  // critical
+            		RunResult.getBreachDates(connector, breach, runType, 
+            				liquidityBreachLimit, "", liquidityLimit.getMetal()) // breachDates
+            		);
+            intradayResults.add(runResult);
+        }
+        for ( RunResult runResult : intradayResults) {
+            connector.saveRunResult(runResult);        	
+        }
+        return intradayResults;
+    }
 
         for ( RunResult runResult : intradayResults) {
             connector.saveRunResult(runResult);        	
