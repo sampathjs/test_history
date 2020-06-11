df0f4e72b3388ec205bcc61599d9f3f1ade58526
JOHNSONMATTHEY\SharmD02 <deepika.sharma@matthey.com>
2020-06-11 19:46:56 +0100

    this class gets initialised and closed from MetalTransferTriggerScript so the log details should go inside the MetalTransferTriggerScript

--- Java/MetalTransfer_JVS/src/com/olf/jm/metalstransfer/trigger/TriggerCancelMetalTransfer.java
+++ Java/MetalTransfer_JVS/src/com/olf/jm/metalstransfer/trigger/TriggerCancelMetalTransfer.java
@@ -18,7 +18,7 @@ public class TriggerCancelMetalTransfer extends MetalTransferTriggerScript {
 	public TriggerCancelMetalTransfer() throws OException {
 	}	
 	protected void init() throws OException {
-		Logging.init(this.getClass(), "MetalTransfer", Constants.LOG_FILE);
+		Logging.init(this.getClass(), "MetalTransfer", Constants.LOG_FILE_NAME);
 	}
 	//No Cash trades available, stamp status to "Succeeded" in User Table
 	protected String processTranNoCashTrade(int trannum) throws OException {
