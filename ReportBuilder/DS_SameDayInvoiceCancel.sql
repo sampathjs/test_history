8f7a28eb7cd171a3457feba5e937abcc8ebeb38e
Arjit.Agrawal <arjit.agrawal@matthey.com>
2020-06-26 19:28:28 +0530

    Removing JOIN condition on stldoc_header_hist_id column (not working in V17)

--- ReportBuilder/DS_SameDayInvoiceCancel.sql
+++ ReportBuilder/DS_SameDayInvoiceCancel.sql
@@ -2,8 +2,8 @@ SELECT ab.deal_tracking_num ,ab.trade_date ,ab.external_bunit, sh.doc_issue_date
 FROM ab_Tran ab  
 JOIN stldoc_details_hist sd ON sd.tran_num=ab.tran_num 
 JOIN stldoc_header_hist sh ON sd.document_num=sh.document_num 
-JOIN stldoc_info_h sti ON sti.document_num=sh.document_num AND sti.stldoc_hdr_hist_id = sh.stldoc_hdr_hist_id
-JOIN stldoc_info_h sti1 ON sti1.document_num=sh.document_num AND sti.stldoc_hdr_hist_id = sh.stldoc_hdr_hist_id
+JOIN stldoc_info_h sti ON sti.document_num=sh.document_num 
+JOIN stldoc_info_h sti1 ON sti1.document_num=sh.document_num 
 WHERE ab.current_flag=1 AND ab.internal_bunit in (20006,20008) --PMM UK, JM PM Ltd
 AND ab.toolset in (9,10,15)		-- Cash, FX,COMSWAP
 AND ab.cflow_type NOT IN (SELECT id_number FROM cflow_type WHERE name LIKE 'Metal Rentals%')
@@ -20,8 +20,8 @@ SELECT ab.deal_tracking_num ,ab.trade_date ,ab.external_bunit, sh.doc_issue_date
 FROM ab_Tran ab 
 JOIN stldoc_details_hist sd ON sd.tran_num=ab.tran_num 
 JOIN stldoc_header_hist sh ON sd.document_num=sh.document_num 
-JOIN stldoc_info_h sti ON sti.document_num=sh.document_num AND sti.stldoc_hdr_hist_id = sh.stldoc_hdr_hist_id
-JOIN stldoc_info_h sti1 ON sti1.document_num=sh.document_num AND sti.stldoc_hdr_hist_id = sh.stldoc_hdr_hist_id
+JOIN stldoc_info_h sti ON sti.document_num=sh.document_num 
+JOIN stldoc_info_h sti1 ON sti1.document_num=sh.document_num 
 where ab.current_flag=1  AND ab.internal_bunit in (20006,20008) --PMM UK, JM PM Ltd
 AND ab.toolset in (9,10,15)  -- Cash, FX,COMSWAP
 AND ab.cflow_type NOT IN (SELECT id_number FROM cflow_type WHERE name LIKE 'Metal Rentals%')
