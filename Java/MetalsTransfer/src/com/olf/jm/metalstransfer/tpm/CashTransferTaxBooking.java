9ca1e283689fdbdf64185a539f04d03dfa291572
Nitesh1225 <52274798+Nitesh1225@users.noreply.github.com>
2020-06-29 22:53:25 +0530

    Updates

--- Java/MetalsTransfer/src/com/olf/jm/metalstransfer/tpm/CashTransferTaxBooking.java
+++ Java/MetalsTransfer/src/com/olf/jm/metalstransfer/tpm/CashTransferTaxBooking.java
@@ -106,7 +106,7 @@ public class CashTransferTaxBooking extends AbstractProcessStep {
             	Logging.info("Process for transaction " + tranNum + " was skipped as the latest tran status is "+transactionStatus);            
             }else{
             Table returnt = process(context, process, tranNum);
-            Logging.info("Completed transaction " + tranNum);
+	    Logging.info("Completed transaction " + tranNum);
             return returnt;
             }
 			
