5f1b621ec99fd6c501a63fa87b2948abcb6945f7
JOHNSONMATTHEY\BadcoC01 <Charles.badcock@matthey.com>
2020-07-02 12:27:36 +0100

    Revert "Revert "Revert "update from uat session"""
    This reverts commit 8dab93f05c0b5dc8391347811bbe3e32d675095d.

--- StandingDataSQL/USER_jm_balance_line_us_SR348479.sql
+++ StandingDataSQL/USER_jm_balance_line_us_SR348479.sql
@@ -11,41 +11,31 @@ go
 -- renamed line for seperated account
 -- update lines for dependent formulas using seperated accounts
 
-update USER_jm_balance_line_us set  balance_line = '145.03' , description = 'L145.03 Stock US WW - SPONGE', display_order = 438 where  id = 29
-
+insert into USER_jm_balance_line_us (id,balance_line,description,formula,display_order,display_in_drilldown)
+values (83,'145.04','L145.04 West Whiteland - ING/GRA','-R83',439,'')
 go
 
 insert into USER_jm_balance_line_us (id,balance_line,description,formula,display_order,display_in_drilldown)
-values (83,'145.04','L145.04 Stock US WW - GRAIN','-R83',439,'')
-go
-
-insert into USER_jm_balance_line_us (id,balance_line,description,formula,display_order,display_in_drilldown)
-values (84,'145.05','L145.05 Stock US WD - INGOT','-R84',440,'')
+values (84,'145.05','L145.05 West Deptford - Sponge','-R84',440,'')
 go
 
 
 insert into USER_jm_balance_line_us (id,balance_line,description,formula,display_order,display_in_drilldown)
-values (85,'145.06','L145.06 Stock US WD - SPONGE','-R85',442,'')
+values (85,'145.06','L145.06 West Deptford - ING/GRA','-R85',441,'')
 go
 
 
-insert into USER_jm_balance_line_us (id,balance_line,description,formula,display_order,display_in_drilldown)
-values (86,'145.07','L145.07 Stock US WD - GRAIN','-R86',443,'')
-
-go
-
-insert into USER_jm_balance_line_us (id,balance_line,description,formula,display_order,display_in_drilldown)
-values (87,'145.08','L145.08 Stock US WD - INGOT','-R87',444,'')
+update USER_jm_balance_line_us set  balance_line = '145.03' , description = 'L145.03 West Whiteland - Sponge', display_order = 438 where  id = 29
 
 
 go
 
 -- update line(s) for dependent formula(s) using seperated accounts
-update USER_jm_balance_line_us set formula = '+(R28,R29,R30,R31,R32,R33,R52,R53,R69,R70,R71,R72,R73,R74,R76,R78,R81,R83,R84,R85,R86,R87)' where  id = 34
+update USER_jm_balance_line_us set formula = '+(R28,R29,R30,R31,R32,R33,R52,R53,R69,R70,R71,R72,R73,R74,R76,R78,R81,R83,R84,R85)' where  id = 34
 
 go
 
-update USER_jm_balance_line_us set formula = '*(/(+(R29,R83,R84,R85,R86,R87),R50), 100)' where  id = 41
+update USER_jm_balance_line_us set formula = '*(/(+(R29,R83,R84,R85),R50), 100)' where  id = 41
 
 
 --################# END US LOCATION/FORM GROUPING UPDATE
