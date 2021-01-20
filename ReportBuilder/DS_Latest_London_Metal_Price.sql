c6f197a8aafc58b7e2a1b91f91dec5c012c0a2ca
JOHNSONMATTHEY\FernaI01 <ivan.fernandes@matthey.com>
2021-01-20 15:25:58 +0000

    Added comments for the IDs

--- ReportBuilder/DS_Latest_London_Metal_Price.sql
+++ ReportBuilder/DS_Latest_London_Metal_Price.sql
@@ -3,6 +3,6 @@ index_id, price
 from
  idx_historical_prices i
  where 
-i.ref_source = 20011
+i.ref_source = 20011 -- London Opening
 and i.reset_date =  (select max(h2.reset_date) from idx_historical_prices h2 where h2.index_id in (1020058, 1020060, 1020061, 1020065, 1020069, 1020077)  and h2.ref_source = 20011)
-and i.index_id in (1020058, 1020060, 1020061, 1020065, 1020069, 1020077)
+and i.index_id in (1020058, 1020060, 1020061, 1020065, 1020069, 1020077) -- XOS.USD, XRU.USD,XPD.USD, XPT.USD, XIR.USD,XRH.USD
