1443b3d652a79852f049e13884c52fc6ab7dae80
BhardG01 <BhardG01@WEAZRDS05.johnsonmatthey.com>
2021-03-18 12:33:06 +0000

    Updated the Code for EPI-1647 Alerts Priority Change Implementation and DeActivation of Some Alerts

--- Java/EOD_JVS/src/com/jm/tpm/support/util/TPMExecuteQueryChecks.java
+++ Java/EOD_JVS/src/com/jm/tpm/support/util/TPMExecuteQueryChecks.java
@@ -34,61 +34,20 @@ public class TPMExecuteQueryChecks implements IScript {
     	}
 		
 		Table queryList = Util.NULL_TABLE;
-		Table tOutput = Util.NULL_TABLE;       
+		Table tOutput = Util.NULL_TABLE; 
+		Table priorityList = Util.NULL_TABLE; 
         
         try {
         	repository = new ConstRepository(CONTEXT, SUBCONTEXT);
         	
         	Logging.info("Script execution starts...");
-        	queryList= fetchQueryList();
-            
-            int rows = queryList.getNumRows();
-            if (rows == 0) {
-            	Logging.info("No rows retrieved from USER_generic_wflow_query_list table");
-            	return;
-            }
-
-            Logging.info(String.format("%d rows retrieved from USER_generic_wflow_query_list table", rows));
-            for (int row = 1; row <= rows; row++) {
-            	String query = queryList.getString("query", row);
-            	String queryName = queryList.getString("query_name", row);
-            	int exactRows = queryList.getInt("exact_expected_rows", row);
-            	int maxRows = queryList.getInt("max_expected_rows", row);
-            	Logging.info(String.format("Running loop for query name - %s, query - %s, exact_expected_rows - %d, max_expected_rows - %d", queryName, query, exactRows, maxRows));
-            	
-            	try {
-            		tOutput = executeQuery(query, queryName);
-    				if (Table.isTableValid(tOutput) != 1) {
-    					Logging.info(String.format("Invalid table retrieved for query (%s), moving to next query", queryName));
-    					continue;
-    				}
-    				
-    				int outputRows = tOutput.getNumRows();
-    				Logging.info(String.format("Non-zero rows (%d) retrieved for query (%s)", outputRows, queryName));
-    				
-    				/**
-    				 * If Exact_Rows = 0 AND Max_Rows = 0, send email if Output_Rows > 0
-    				 * If Exact_Rows > 0, send email if Output_Rows != Exact_Rows 
-    				 * If Exact_Rows = 0 AND Max_Rows > 0, send email if Output_Rows > Max_Rows
-    				 */
-    				if ((exactRows == 0 && maxRows == 0 && outputRows > 0)
-    						|| (exactRows > 0 && outputRows > 0 && outputRows != exactRows)
-    						|| (exactRows == 0 && maxRows > 0 && outputRows > maxRows)) {
-    					Logging.info(String.format("Output rows (%d) doesn't satisfy the criteria (for exact_rows & max_rows) setup in user table", outputRows));
-    					Logging.info(String.format("Sending email to Support team for query (%s)", queryName));
-    					sendEmail(tOutput, queryName);
-    					
-    				} else {
-    					Logging.info(String.format("Output rows (%d) satisfy the criteria (for exact_rows & max_rows) setup in user table, moving to next query", outputRows));
-    				}
-    				
-            	} finally {
-            		if (Table.isTableValid(tOutput) == 1) {
-            			tOutput.destroy();
-                	}
-            	}
-            }
-        	
+        	priorityList = fetchPriorityList();
+        	int rows = priorityList.getNumRows();
+        	for (int row = 1; row <= rows; row++) { 
+        		queryList= fetchQueryList(priorityList.getInt("priority", row));
+        		processQueries(queryList);
+        	}
+        	 
             Logging.info("Script execution ends...");
             
         } catch (OException oe) {
@@ -103,6 +62,143 @@ public class TPMExecuteQueryChecks implements IScript {
         }
 	}
 	
+	private void processQueries(Table queryList) throws OException { 
+		Table tOutput = Util.NULL_TABLE;       
+		 int rows = queryList.getNumRows();
+         if (rows == 0) {
+         	Logging.info("No rows retrieved from USER_generic_wflow_query_list table");
+         	return;
+         }
+		 Logging.info(String.format("%d rows retrieved from USER_generic_wflow_query_list table", rows));
+         for (int row = 1; row <= rows; row++) {
+         	String query = queryList.getString("query", row);
+         	String queryName = queryList.getString("query_name", row);
+         	int exactRows = queryList.getInt("exact_expected_rows", row);
+         	int maxRows = queryList.getInt("max_expected_rows", row);
+         	Logging.info(String.format("Running loop for query name - %s, query - %s, exact_expected_rows - %d, max_expected_rows - %d", queryName, query, exactRows, maxRows));
+         	
+         	try {
+         		tOutput = executeQuery(query, queryName);
+ 				if (Table.isTableValid(tOutput) != 1) {
+ 					Logging.info(String.format("Invalid table retrieved for query (%s), moving to next query", queryName));
+ 					continue;
+ 				}
+ 				
+ 				int outputRows = tOutput.getNumRows();
+ 				Logging.info(String.format("Non-zero rows (%d) retrieved for query (%s)", outputRows, queryName));
+ 				
+ 				/**
+ 				 * If Exact_Rows = 0 AND Max_Rows = 0, send email if Output_Rows > 0
+ 				 * If Exact_Rows > 0, send email if Output_Rows != Exact_Rows 
+ 				 * If Exact_Rows = 0 AND Max_Rows > 0, send email if Output_Rows > Max_Rows
+ 				 */
+ 				if ((exactRows == 0 && maxRows == 0 && outputRows > 0)
+ 						|| (exactRows > 0 && outputRows > 0 && outputRows != exactRows)
+ 						|| (exactRows == 0 && maxRows > 0 && outputRows > maxRows)) {
+ 					Logging.info(String.format("Output rows (%d) doesn't satisfy the criteria (for exact_rows & max_rows) setup in user table", outputRows));
+ 					Logging.info(String.format("Sending email to Support team for query (%s)", queryName));
+ 					sendEmail(tOutput, queryName);
+ 					
+ 				} else {
+ 					Logging.info(String.format("Output rows (%d) satisfy the criteria (for exact_rows & max_rows) setup in user table, moving to next query", outputRows));
+ 				}
+ 				
+         	} finally {
+         		if (Table.isTableValid(tOutput) == 1) {
+         			tOutput.destroy();
+             	}
+         	}
+         }
+		
+	}
+
+	private Table fetchPriorityList() throws OException {
+		Table priorityList = Util.NULL_TABLE; 
+        int retval;
+        
+        try {
+        	priorityList= Table.tableNew();
+        	String sqlQuery = "SELECT DISTINCT(u.priority) priority FROM USER_generic_wflow_query_list u WHERE u.active = 1 ";
+        	Logging.info(String.format("Executing SQL query - %s", sqlQuery));
+        	retval = DBaseTable.execISql(priorityList, sqlQuery);
+        	
+            if (retval != OLF_RETURN_CODE.OLF_RETURN_SUCCEED.toInt()) {
+            	throw new OException(String.format("Error in executing query - %s", sqlQuery));
+            } 
+            
+        } catch (OException oe) {
+        	throw new OException(String.format("Error in executing query. Error Message - %s", oe.getMessage()));        	
+		} 
+        
+        return priorityList;
+	}
+
+
+
+	private Table fetchQueryList(int priority) throws OException {
+		Table queryList = Util.NULL_TABLE;
+		Table tQueries = Util.NULL_TABLE;
+        int retval;
+        
+        try {
+        	queryList= Table.tableNew();
+        	String sqlQuery = "SELECT u.* FROM USER_generic_wflow_query_list u WHERE u.active = 1 AND u.priority ="+priority+"  ORDER BY u.query_name,sequence";
+        	Logging.info(String.format("Executing SQL query - %s", sqlQuery));
+        	retval = DBaseTable.execISql(queryList, sqlQuery);
+        	
+            if (retval != OLF_RETURN_CODE.OLF_RETURN_SUCCEED.toInt()) {
+            	throw new OException(String.format("Error in executing query - %s", sqlQuery));
+            }
+            
+            tQueries = queryList.cloneTable();
+            tQueries.delCol("active");
+            tQueries.delCol("sequence");
+            tQueries.delCol("id");
+            
+            String queryName = null;
+            String fullQuery = "";
+            
+            int rows = queryList.getNumRows();
+            for (int row = 1; row <= rows; row++) {
+            	String currQueryName = queryList.getString("query_name", row);
+            	String partQuery = queryList.getString("query", row);
+            	
+            	if (queryName != null && !queryName.equals(currQueryName)) {
+            		int rowNum = tQueries.addRow();
+            		tQueries.setInt("exact_expected_rows", rowNum, queryList.getInt("exact_expected_rows", (row - 1)));
+            		tQueries.setInt("max_expected_rows", rowNum, queryList.getInt("max_expected_rows", (row - 1)));
+            		tQueries.setString("query", rowNum, fullQuery);
+            		tQueries.setString("query_name", rowNum, queryName);
+            		
+            		queryName = currQueryName;
+            		fullQuery = partQuery + " ";
+            		
+            	} else {
+            		fullQuery += partQuery + " ";
+            		queryName = currQueryName;
+            	}
+            	
+            	if (row == rows) {
+            		int rowNum = tQueries.addRow();
+            		tQueries.setInt("exact_expected_rows", rowNum, queryList.getInt("exact_expected_rows", row));
+            		tQueries.setInt("max_expected_rows", rowNum, queryList.getInt("max_expected_rows", row));
+            		tQueries.setString("query", rowNum, fullQuery);
+            		tQueries.setString("query_name", rowNum, queryName);
+            	}
+            }
+            
+        } catch (OException oe) {
+        	throw new OException(String.format("Error in executing query. Error Message - %s", oe.getMessage()));
+        	
+		} finally {
+        	if (Table.isTableValid(queryList) == 1) {
+        		queryList.destroy();
+        	}
+        }
+        
+        return tQueries;
+	}
+
 	/**
 	 * Method to fetch all active queries present in USER_generic_wflow_query_list table.
 	 * @return queryList
