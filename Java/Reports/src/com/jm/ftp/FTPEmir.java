c6847371cf62f2c97cef6b2fe2b5acb1a5eac61d
JOHNSONMATTHEY\FernaI01 <ivan.fernandes@matthey.com>
2021-03-24 16:04:10 +0000

    updated hard coded variable go use const repo variable

--- Java/Reports/src/com/jm/ftp/FTPEmir.java
+++ Java/Reports/src/com/jm/ftp/FTPEmir.java
@@ -75,11 +75,11 @@ public class FTPEmir extends FTP
 			
 			File fileEMIR = new File(strFilePathFileName);
 			
-			String strFileName = strFilePathFileName.substring(strFilePathFileName.indexOf("RPRP2304"), strFilePathFileName.length());
+			String strFileName = strFilePathFileName.substring(strFilePathFileName.indexOf(strEmirUser.toUpperCase()), strFilePathFileName.length());
 			
 			String strFileNameTmp =  "TMP_" + strFileName;
 			
-			String strFilePath = strFilePathFileName.substring(0, strFilePathFileName.indexOf("RPRP2304"));
+			String strFilePath = strFilePathFileName.substring(0, strFilePathFileName.indexOf(strEmirUser.toUpperCase()));
 			
 			String strFilePathFileNameTmp =  strFilePath + strFileNameTmp;
 			
