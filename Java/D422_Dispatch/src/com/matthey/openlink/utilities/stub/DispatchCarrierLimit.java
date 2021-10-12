7dd7a3b017842acb79b4bb8b005f6618ab1497d5
BhardG01 <BhardG01@WEAZRDS03.johnsonmatthey.com>
2021-10-12 17:05:25 +0100

    Updated the code for Dispatch Carer Limit

--- Java/D422_Dispatch/src/com/matthey/openlink/utilities/stub/DispatchCarrierLimit.java
+++ Java/D422_Dispatch/src/com/matthey/openlink/utilities/stub/DispatchCarrierLimit.java
@@ -4,11 +4,10 @@ import com.olf.embedded.application.Context;
 import com.olf.embedded.application.EnumScriptCategory;
 import com.olf.embedded.application.ScriptCategory;
 import com.olf.embedded.tpm.AbstractProcessStep;
+import com.olf.jm.logging.Logging;
 import com.olf.openjvs.OException;
 import com.olf.openjvs.Ref;
 import com.olf.openjvs.enums.SHM_USR_TABLES_ENUM;
-import com.olf.openrisk.market.EnumElementType;
-import com.olf.openrisk.market.ForwardCurve;
 import com.olf.openrisk.staticdata.Person;
 import com.olf.openrisk.table.Table;
 import com.olf.openrisk.table.TableFactory;
@@ -21,50 +20,87 @@ import com.olf.openrisk.trading.Leg;
 import com.olf.openrisk.trading.Transaction;
 
 
-/*
+/*/*
  * History:
- * 2021-10-12	V1.2   BhardG01	- PBI000000002306 - Issue entering order
+ * 2020-03-25	V1.1	 BhardG01	- WO0000000064879 - Physical dispatch risk check - based on value & carrier 
+ * 2021-10-12	V1.2     BhardG01	- PBI000000002306 - Issue entering order
  */
 @ScriptCategory({ EnumScriptCategory.TpmStep })
 public class DispatchCarrierLimit extends AbstractProcessStep {
+	
 
-	@Override 
+	private static final String CONST_REPO_CONTEXT = "Support"; 
+	private static final String CONST_REPO_SUBCONTEXT="DispatchCarrierLimit";
+
+	@Override
 	public Table execute(Context context, Process process, Token token,
 			Person submitter, boolean transferItemLocks, Variables variables) {
-		Variable dispatchLimit = process.getVariable("dispatchFlagLimit"); 
+		
+		
+		Logging.init(context, this.getClass(),  CONST_REPO_CONTEXT, CONST_REPO_SUBCONTEXT);
+		
+		 
+		//Collateral_Value
+		try{  
+		Variable dispatchFlagLimit = process.getVariable("dispatchFlagLimit"); 
+		Variable dispathCarrierLimit = process.getVariable("Dispath_Carrier_Limit"); 
+		Variable totalDealValue = process.getVariable("Total_Deal_Value"); 
+		Variable dispatchAssignment = process.getVariable("Dispatch_Assignment"); 
 		int tranNum = process.getVariable("TranNum").getValueAsInt(); 
 		Transaction tranPtr = context.getTradingFactory().retrieveTransactionById(tranNum);
-		dispatchLimit.setValue(isPhysicalDispatchRiskLimitBreached( tranPtr,  context));
-		process.setVariable(dispatchLimit);
-		boolean isPhyDispatchLimitBreached = isPhysicalDispatchRiskLimitBreached( tranPtr,  context);
+		
+		dispatchFlagLimit.setValue(isPhysicalDispatchRiskLimitBreached( tranPtr,  context));
+		String careerStr=  tranPtr.getField("Carrier").getValueAsString();
 
+		dispathCarrierLimit.setValue(getDispatchCareerApplicableLimit(careerStr, context));
+
+		totalDealValue.setValue(getTotalDealValue( tranPtr, context)); 
+
+		dispatchAssignment.setValue(1);
+		process.setVariable(dispatchFlagLimit);
+		process.setVariable(dispathCarrierLimit);
+		process.setVariable(totalDealValue);
+		process.setVariable(dispatchAssignment);
+		}catch(Exception ex){
+			throw new RuntimeException("Retrieve Collateral failed",ex);
+		} finally{
+			Logging.close();
+		}  
 		return variables.asTable().cloneData();
 	}
 
-	private boolean isPhysicalDispatchRiskLimitBreached(Transaction tranPtr, Context context) {
-			String lastProjectionIndex = ""; 
-			Double dailyVolume = 0.0;
-			Double dailyPrice = 0.0;
-			Double physicalDispathValue = 0.0;
-			ForwardCurve projIndex = null;
-			 
-			String careerStr=  tranPtr.getField("Carrier").getValueAsString(); ; 
+	private Double getTotalDealValue(Transaction tranPtr, Context context) throws OException {
+			 Double dailyPrice,dailyVolume = 0.0;
+			Double physicalDispathValue = 0.0; 
+			int lastProjectionIndexInt ;
 			for (Leg currentLeg : tranPtr.getLegs()){
 				if(currentLeg.isPhysicalCommodity()){ 
 				
 				dailyVolume = currentLeg.getValueAsDouble( EnumLegFieldId.DailyVolume); 
-				lastProjectionIndex = currentLeg.getField(EnumLegFieldId.ProjectionIndex).getValueAsString();
-				projIndex = (ForwardCurve)tranPtr.getPricingDetails().getMarket().getElement(EnumElementType.ForwardCurve, lastProjectionIndex); 
-				int lastProjectionIndexInt = currentLeg.getField(EnumLegFieldId.ProjectionIndex).getValueAsInt(); 
-				//dailyPrice   = projIndex.getDirectParentIndexes().get(0).getGridPoints().getGridPoint("Spot").getValue(EnumGptField.EffInput);
-			 	//physicalDispathValue = physicalDispathValue + 	dailyPrice*	dailyVolume;
-			 	
-			 	try {
-					dailyPrice   = getHistoricalPrices(lastProjectionIndexInt, context);
-				} catch (OException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				lastProjectionIndexInt = currentLeg.getField(EnumLegFieldId.ProjectionIndex).getValueAsInt();
+			 	dailyPrice   = getHistoricalPrices(lastProjectionIndexInt, context);
+				physicalDispathValue = physicalDispathValue + 	dailyPrice*	dailyVolume;
+						 
 				}
+			}
+	 		  
+			return physicalDispathValue;
+		
+	}
+
+	private boolean isPhysicalDispatchRiskLimitBreached(Transaction tranPtr, Context context) throws OException {
+			Double dailyPrice,dailyVolume = 0.0;
+			Double physicalDispathValue = 0.0; 
+			String careerStr=  tranPtr.getField("Carrier").getValueAsString();
+			int lastProjectionIndexInt ;
+			 
+			for (Leg currentLeg : tranPtr.getLegs()){
+				if(currentLeg.isPhysicalCommodity()){ 
+				
+				dailyVolume = currentLeg.getValueAsDouble( EnumLegFieldId.DailyVolume); 
+				lastProjectionIndexInt = currentLeg.getField(EnumLegFieldId.ProjectionIndex).getValueAsInt();
+				dailyPrice   = getHistoricalPrices(lastProjectionIndexInt, context);
+				 
 			 	physicalDispathValue = physicalDispathValue + 	dailyPrice*	dailyVolume;
 						 
 				}
@@ -90,7 +126,8 @@ public class DispatchCarrierLimit extends AbstractProcessStep {
 		return price;
 	}
 
-	private int getRefSource(int lastProjectionIndexInt, Context context) throws OException {
+	private int getRefSource(int lastProjectionIndexInt, Context context) throws OException 
+	{
 		
 		Table queryList = context.getTableFactory().createTable();          
     	String queryStr = "SELECT ref_source,index_name FROM  idx_def WHERE index_id=" + lastProjectionIndexInt +" AND db_status=1" ; 
@@ -113,10 +150,10 @@ public class DispatchCarrierLimit extends AbstractProcessStep {
 
 		TableFactory tf = context1.getTableFactory();  
 		Table queryList = tf.createTable();  
-        	String queryStr =  "SELECT upperlimit FROM  USER_jm_dispatch_carrier_limit   WHERE carrier= '" + carrier +"'" ;
+        	String queryStr =  "SELECT limit FROM  USER_jm_dispatch_carrier_limit   WHERE carrier= '" + carrier +"'" ;
         	queryList = context1.getIOFactory().runSQL(queryStr); 
-        	Double upperlimit = queryList.getDouble("upperlimit", 0);
-			return upperlimit;
+        	Double limit = queryList.getDouble("limit", 0);
+			return limit;
 	}
 	
 }
