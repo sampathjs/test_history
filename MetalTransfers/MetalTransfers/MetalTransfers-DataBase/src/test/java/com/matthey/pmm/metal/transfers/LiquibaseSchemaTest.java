d589573f329b31e2bbe5bf2de4d0103c3332977d
Boris Liberman <myemail@321.de>
2022-02-15 21:41:53 +0100

    renamed a test class

--- MetalTransfers/MetalTransfers/MetalTransfers-DataBase/src/test/java/com/matthey/pmm/metal/transfers/LiquibaseSchemaTest.java
+++ MetalTransfers/MetalTransfers/MetalTransfers-DataBase/src/test/java/com/matthey/pmm/metal/transfers/LiquibaseSchemaTest.java
@@ -1,27 +1,34 @@
 package com.matthey.pmm.metal.transfers;
 
+import com.matthey.pmm.endur.database.repository.AbTranRepository;
 import com.matthey.pmm.metal.transfers.repository.UserJmFormRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = LbSchemaTest.LbSchemaTestConfig.class)
-public class LbSchemaTest {
+@SpringBootTest(classes = LiquibaseSchemaTest.LbSchemaTestConfig.class)
+public class LiquibaseSchemaTest {
 
     @Configuration
     @EnableAutoConfiguration
     @AutoConfigureDataJpa
-    @ComponentScan(basePackages = {"com.matthey.pmm.endur.database.repository",
-            "com.matthey.pmm.metal.transfers.repository"})
+    @EnableJpaRepositories(basePackages = {"com.matthey.pmm"})
+    @EntityScan(basePackages = {"com.matthey.pmm"})
+    @ComponentScan(basePackages = {"com.matthey.pmm"})
     static class LbSchemaTestConfig {
     }
+
+    @Autowired
+    AbTranRepository abTranRepository;
 
     @Autowired
     UserJmFormRepository userJmFormRepository;
